38df94451b3ddd3f1a04f96e4f7a8a7f814fab37
Will Saunders <will.saunders@ukaea.uk>
2022-07-19 10:33:17 +0100

    added buffer which wraps a device buffer and a host buffer with copy operations

--- include/compute_target.hpp
+++ include/compute_target.hpp
@@ -139,6 +139,51 @@ public:
   }
 };
 
+template <typename T> class BufferDeviceHost {
+private:
+public:
+  SYCLTarget &sycl_target;
+  size_t size;
+
+  BufferDevice<T> d_buffer;
+  BufferHost<T> h_buffer;
+
+  ~BufferDeviceHost(){};
+  BufferDeviceHost(SYCLTarget &sycl_target, size_t size)
+      : sycl_target(sycl_target), size(size), d_buffer(sycl_target, size),
+        h_buffer(sycl_target, size){};
+
+  inline size_t size_bytes() { return this->size * sizeof(T); }
+  inline int realloc_no_copy(const size_t size) {
+    this->d_buffer.realloc_no_copy(size);
+    this->h_buffer.realloc_no_copy(size);
+    this->size = size;
+    return this->size;
+  }
+
+  inline void host_to_device() {
+    this->sycl_target.queue
+        .memcpy(this->d_buffer.ptr, this->h_buffer.ptr, this->size_bytes())
+        .wait();
+  }
+
+  inline void device_to_host() {
+    this->sycl_target.queue
+        .memcpy(this->h_buffer.ptr, this->d_buffer.ptr, this->size_bytes())
+        .wait();
+  }
+
+  inline sycl::event async_host_to_device() {
+    return this->sycl_target.queue.memcpy(
+        this->d_buffer.ptr, this->h_buffer.ptr, this->size_bytes());
+  }
+
+  inline sycl::event async_device_to_host() {
+    return this->sycl_target.queue.memcpy(
+        this->h_buffer.ptr, this->d_buffer.ptr, this->size_bytes());
+  }
+};
+
 } // namespace NESO::Particles
 
 #endif
