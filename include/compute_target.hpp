cad8037b7e7e60a17e1f1681df082200d5fc10f1
Will Saunders <will.saunders@ukaea.uk>
2022-10-05 11:43:52 +0100

    created a class to track the existance of errors thrown in a SYCL kernel

--- include/compute_target.hpp
+++ include/compute_target.hpp
@@ -6,6 +6,7 @@
 #include <CL/sycl.hpp>
 #include <mpi.h>
 #include <stack>
+#include <string>
 
 #include "communication.hpp"
 #include "profiling.hpp"
@@ -426,6 +427,68 @@ public:
     }
   };
 };
+
+/**
+ *  Helper class to propagate errors thrown from inside a SYCL kernel
+ */
+class ErrorPropagate {
+private:
+  SYCLTarget &sycl_target;
+  BufferDeviceHost<int> dh_flag;
+
+public:
+  ~ErrorPropagate(){};
+
+  /**
+   * Create a new instance to track assertions thrown in SYCL kernels.
+   *
+   * @param sycl_target SYCLTarget used for the kernel.
+   */
+  ErrorPropagate(SYCLTarget &sycl_target)
+      : sycl_target(sycl_target), dh_flag(sycl_target, 1) {
+    this->dh_flag.h_buffer.ptr[0] = 0;
+    this->dh_flag.host_to_device();
+  };
+
+  /**
+   * Get the int device pointer for use in a SYCL kernel. This pointer should
+   * be incremented atomically with some positive integer to indicate an error
+   * occured in the kernel.
+   */
+  inline int *get_device_ptr() { return this->dh_flag.d_buffer.ptr; }
+
+  /**
+   * Check the stored integer. If the integer is non-zero throw an error with
+   * the passed message.
+   *
+   * @param error_msg Message to throw if stored integer is non-zero.
+   */
+  inline void check_and_throw(const char *error_msg) {
+    NESOASSERT(this->get_flag() == 0, error_msg);
+  }
+
+  /**
+   *  Get the current value of the flag on the host.
+   *
+   *  @returns flag.
+   */
+  inline int get_flag() {
+    this->dh_flag.device_to_host();
+    return this->dh_flag.h_buffer.ptr[0];
+  }
+};
+
+/*
+ *  Helper preprocessor macro to atomically increment the pointer in an
+ *  ErrorPropagate class.
+ */
+#define NESO_KERNEL_ASSERT(expr, ep_ptr)                                       \
+  if (!(expr)) {                                                               \
+    sycl::atomic_ref<int, sycl::memory_order::relaxed,                         \
+                     sycl::memory_scope::device>                               \
+        neso_error_atomic(ep_ptr[0]);                                          \
+    neso_error_atomic.fetch_add(1);                                            \
+  }
 
 } // namespace NESO::Particles
 
---
+++ test/test_error_propagate.cpp
@@ -0,0 +1,27 @@
+#include <CL/sycl.hpp>
+#include <gtest/gtest.h>
+#include <neso_particles.hpp>
+
+using namespace NESO::Particles;
+
+TEST(ErrorPropagate, Flag) {
+  SYCLTarget sycl_target{0, MPI_COMM_WORLD};
+
+  // create an object to track that an error should be thrown
+  ErrorPropagate ep(sycl_target);
+
+  auto k_ep = ep.get_device_ptr();
+  // get the kernel parameter
+
+  sycl_target.queue
+      .submit([&](sycl::handler &cgh) {
+        cgh.parallel_for<>(sycl::range<1>(8), [=](sycl::id<1> idx) {
+          // throw an error
+          NESO_KERNEL_ASSERT(false, k_ep);
+        });
+      })
+      .wait_and_throw();
+
+  ASSERT_EQ(ep.get_flag(), 8);
+  // ep.check_and_throw("This should abort.");
+}
