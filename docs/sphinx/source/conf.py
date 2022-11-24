965b21f600fd32e7663adfcc2c34491d22ecaf64
James Cook <cookjws@gmail.com>
2022-11-24 14:38:17 +0000

    Revert "build_branch_docs.sh do tags and main branch only (#7)" (#9)
    This reverts commit 523c69785ef79ddf467283e23a3bbb4fae980191.

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -36,25 +36,6 @@ html_theme_options = {
     "show_nav_level": 3
 }
 
-import git
-import json
-
-repo = git.Repo('.')
-tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-
-def jsonobjectfunc(version):
-    strversion = str(version)
-    urlversion = "https://excalibur-neptune.github.io/NESO-Particles/" + strversion + "/sphinx/html/"
-    return {"version": strversion, "url": urlversion}
-    
-json_contents = [jsonobjectfunc("main")]
-for t in tags:
-    tagobject = jsonobjectfunc(t)
-    json_contents.append(tagobject)
-
-with open('../../switcher.json', 'w') as fh:
-    json.dump(json_contents, fh)
-
 import os
 docs_version = "./docs_version"
 if os.path.exists(docs_version):
