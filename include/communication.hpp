42e864df2529c1d33e5e406260e35dff0d2a279f
Will Saunders <will.saunders@ukaea.uk>
2023-10-20 12:36:23 +0100

    global array implementation progress

--- include/communication.hpp
+++ include/communication.hpp
@@ -104,6 +104,38 @@ public:
   }
 };
 
+namespace {
+template <typename T> constexpr MPI_Datatype map_ctype_mpi_type_inner() {
+  return MPI_DATATYPE_NULL;
+}
+template <> constexpr MPI_Datatype map_ctype_mpi_type_inner<int>() {
+  return MPI_INT;
+}
+template <> constexpr MPI_Datatype map_ctype_mpi_type_inner<int64_t>() {
+  return MPI_INT64_T;
+}
+template <> constexpr MPI_Datatype map_ctype_mpi_type_inner<uint64_t>() {
+  return MPI_UINT64_T;
+}
+template <> constexpr MPI_Datatype map_ctype_mpi_type_inner<double>() {
+  return MPI_DOUBLE;
+}
+
+} // namespace
+
+/**
+ *  For an input data type T get the matching MPI Datatype.
+ *
+ *  @param i Input argument of type T.
+ *  @returns MPI_Datatype that matches the input type T.
+ */
+template <typename T> constexpr MPI_Datatype map_ctype_mpi_type() {
+  constexpr MPI_Datatype t = map_ctype_mpi_type_inner<T>();
+  static_assert(t != MPI_DATATYPE_NULL,
+                "Could not find MPI_Datatype matching passed type.");
+  return t;
+}
+
 } // namespace NESO::Particles
 
 #endif
