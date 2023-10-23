51f5c24687a754120f17e5ba8af8a7ef525fdd37
Will Saunders <will.saunders@ukaea.uk>
2023-10-23 12:48:37 +0100

    mesh pbc innew style particle loop

--- include/boundary_conditions.hpp
+++ include/boundary_conditions.hpp
@@ -5,9 +5,11 @@
 #include <cmath>
 
 #include "domain.hpp"
+#include "loop/particle_loop.hpp"
 #include "particle_dat.hpp"
 #include "profiling.hpp"
 #include "typedefs.hpp"
+#include <memory>
 
 using namespace cl;
 using namespace std;
@@ -24,44 +26,15 @@ private:
   SYCLTargetSharedPtr sycl_target;
   std::shared_ptr<CartesianHMesh> mesh;
   ParticleDatSharedPtr<REAL> position_dat;
-
-public:
-  /// Disable (implicit) copies.
-  CartesianPeriodic(const CartesianPeriodic &st) = delete;
-  /// Disable (implicit) copies.
-  CartesianPeriodic &operator=(CartesianPeriodic const &a) = delete;
-
-  ~CartesianPeriodic(){};
-
-  /**
-   * Construct instance to apply periodic boundary conditions to particles
-   * within the passed ParticleDat.
-   *
-   * @param sycl_target SYCLTarget to use as compute device.
-   * @param mesh CartedianHMesh instance to use a domain for the particles.
-   * @param position_dat ParticleDat containing particle positions.
-   */
-  CartesianPeriodic(SYCLTargetSharedPtr sycl_target,
-                    std::shared_ptr<CartesianHMesh> mesh,
-                    ParticleDatSharedPtr<REAL> position_dat)
-      : sycl_target(sycl_target), mesh(mesh), position_dat(position_dat),
-        d_extents(sycl_target, 3) {
-
-    NESOASSERT(mesh->ndim <= 3, "bad mesh ndim");
-    BufferHost<REAL> h_extents(sycl_target, 3);
-    for (int dimx = 0; dimx < mesh->ndim; dimx++) {
-      h_extents.ptr[dimx] = this->mesh->global_extents[dimx];
-    }
-    sycl_target->queue
-        .memcpy(this->d_extents.ptr, h_extents.ptr, mesh->ndim * sizeof(REAL))
-        .wait_and_throw();
-  };
+  ParticleLoopSharedPtr pbc_loop;
+  bool style_old;
+  bool style_new;
 
   /**
    * Apply periodic boundary conditions to the particle positions in the
    * ParticleDat this instance was created with.
    */
-  inline void execute() {
+  inline void execute_old() {
 
     auto t0 = profile_timestamp();
     auto pl_iter_range = this->position_dat->get_particle_loop_iter_range();
@@ -142,6 +115,109 @@ public:
                 });
           })
           .wait_and_throw();
+    }
+
+    sycl_target->profile_map.inc("CartesianPeriodic", "execute", 1,
+                                 profile_elapsed(t0, profile_timestamp()));
+  }
+
+  inline void execute_new() {
+    NESOASSERT(this->style_new,
+               "Expected to be initialised with new style looping.");
+    NESOASSERT(this->pbc_loop != nullptr, "ParticleLoop not defined.");
+    this->pbc_loop->execute();
+  }
+
+public:
+  /// Disable (implicit) copies.
+  CartesianPeriodic(const CartesianPeriodic &st) = delete;
+  /// Disable (implicit) copies.
+  CartesianPeriodic &operator=(CartesianPeriodic const &a) = delete;
+
+  ~CartesianPeriodic(){};
+
+  /**
+   * Construct instance to apply periodic boundary conditions to particles
+   * within the passed ParticleDat.
+   *
+   * @param sycl_target SYCLTarget to use as compute device.
+   * @param mesh CartedianHMesh instance to use a domain for the particles.
+   * @param position_dat ParticleDat containing particle positions.
+   */
+  CartesianPeriodic(SYCLTargetSharedPtr sycl_target,
+                    std::shared_ptr<CartesianHMesh> mesh,
+                    ParticleDatSharedPtr<REAL> position_dat)
+      : sycl_target(sycl_target), mesh(mesh), position_dat(position_dat),
+        d_extents(sycl_target, 3), style_old(true), style_new(false) {
+
+    NESOASSERT(mesh->ndim <= 3, "bad mesh ndim");
+    BufferHost<REAL> h_extents(sycl_target, 3);
+    for (int dimx = 0; dimx < mesh->ndim; dimx++) {
+      h_extents.ptr[dimx] = this->mesh->global_extents[dimx];
+    }
+    sycl_target->queue
+        .memcpy(this->d_extents.ptr, h_extents.ptr, mesh->ndim * sizeof(REAL))
+        .wait_and_throw();
+  };
+
+  /**
+   * Construct instance to apply periodic boundary conditions to particles
+   * within the passed ParticleDat.
+   *
+   * @param mesh CartedianHMesh instance to use a domain for the particles.
+   * @param particle_group ParticleGroup to apply periodic boundary conditions
+   * to.
+   */
+  CartesianPeriodic(std::shared_ptr<CartesianHMesh> mesh,
+                    ParticleGroupSharedPtr particle_group)
+      : mesh(mesh), sycl_target(particle_group->sycl_target),
+        d_extents(particle_group->sycl_target, 3), style_old(false),
+        style_new(true) {
+
+    NESOASSERT(mesh->ndim <= 3, "bad mesh ndim");
+    BufferHost<REAL> h_extents(sycl_target, 3);
+    for (int dimx = 0; dimx < mesh->ndim; dimx++) {
+      h_extents.ptr[dimx] = this->mesh->global_extents[dimx];
+    }
+    sycl_target->queue
+        .memcpy(this->d_extents.ptr, h_extents.ptr, mesh->ndim * sizeof(REAL))
+        .wait_and_throw();
+
+    auto position_sym = particle_group->position_dat->sym;
+    const int k_ndim = this->mesh->ndim;
+    NESOASSERT(((k_ndim > 0) && (k_ndim < 4)), "Bad number of dimensions");
+    const auto k_extents = this->d_extents.ptr;
+
+    this->pbc_loop = particle_loop(
+        "CartesianPeriodicPBC", particle_group,
+        [=](auto P) {
+          for (int dimx = 0; dimx < k_ndim; dimx++) {
+            const REAL pos = P[dimx];
+            // offset the position in the current dimension to be
+            // positive by adding a value times the extent
+            const REAL n_extent_offset_real = ABS(pos);
+            const REAL tmp_extent = k_extents[dimx];
+            const INT n_extent_offset_int = n_extent_offset_real + 2.0;
+            const REAL pos_fmod =
+                fmod(pos + n_extent_offset_int * tmp_extent, tmp_extent);
+            P[dimx] = pos_fmod;
+          }
+        },
+        Access::write(position_sym));
+  };
+
+  /**
+   * Apply periodic boundary conditions to the particle positions in the
+   * ParticleDat this instance was created with.
+   */
+  inline void execute() {
+    auto t0 = profile_timestamp();
+
+    if (style_old) {
+      this->execute_old();
+    } else {
+      NESOASSERT(style_new, "Expected new stlye looping");
+      this->execute_new();
     }
 
     sycl_target->profile_map.inc("CartesianPeriodic", "execute", 1,
