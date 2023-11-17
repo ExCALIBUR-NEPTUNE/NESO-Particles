2bf6acc4ddec5df099df368896cc1fc53d021439
Will Saunders <will.saunders@ukaea.uk>
2023-11-17 10:37:30 +0000

    added missing inline keyword

--- include/communication.hpp
+++ include/communication.hpp
@@ -129,7 +129,7 @@ template <> constexpr MPI_Datatype map_ctype_mpi_type_inner<double>() {
  *  @param i Input argument of type T.
  *  @returns MPI_Datatype that matches the input type T.
  */
-template <typename T> MPI_Datatype map_ctype_mpi_type() {
+template <typename T> inline MPI_Datatype map_ctype_mpi_type() {
   constexpr MPI_Datatype t = map_ctype_mpi_type_inner<T>();
   NESOASSERT(t != MPI_DATATYPE_NULL,
              "Could not find MPI_Datatype matching passed type.");
