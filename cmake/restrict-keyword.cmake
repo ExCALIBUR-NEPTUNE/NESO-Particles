46f27cbf2671d90721aa009d1da65b4539874215
Will Saunders <will.saunders@ukaea.uk>
2023-11-24 13:12:50 +0000

    added cmake functionality to determine and define C++ restrict keyword

---
+++ cmake/restrict-keyword.cmake
@@ -0,0 +1,40 @@
+# try to find an acceptable CXX restrict keyword if not user provided
+INCLUDE(CheckCXXSourceCompiles)
+
+if(NOT DEFINED RESTRICT)
+    message(STATUS "Detecting restrict keyword")
+    check_cxx_source_compiles(
+        "
+        int f(void *restrict x);
+        int main(void) {return 0;}
+        "
+        HAVERESTRICT
+    )
+    check_cxx_source_compiles(
+        "
+        int f(void * __restrict x);
+        int main(void) {return 0;}
+        "
+        HAVE__RESTRICT
+    )
+    check_cxx_source_compiles(
+        "
+        int f(void * __restrict__ x);
+        int main(void) {return 0;}
+        "
+        HAVE__RESTRICT__
+    )
+
+    if (HAVERESTRICT)
+        set(RESTRICT "restrict")
+    elseif(HAVE__RESTRICT)
+        set(RESTRICT "__restrict")
+    elseif(HAVE__RESTRICT__)
+        set(RESTRICT "__restrict__")
+    else()
+        set(RESTRICT "")
+    endif()
+endif()
+
+message(STATUS "Using restrict keyword: " ${RESTRICT})
+add_compile_definitions(RESTRICT=${RESTRICT})
