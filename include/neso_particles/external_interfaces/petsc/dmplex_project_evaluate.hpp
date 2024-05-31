e520d1a3267ca3436d2498b00be36bfedfe26804
Will Saunders <will.saunders@ukaea.uk>
2024-05-31 11:22:06 +0100

    split dmplex project/evaluate interface from implementations

--- include/neso_particles/external_interfaces/petsc/dmplex_project_evaluate.hpp
+++ include/neso_particles/external_interfaces/petsc/dmplex_project_evaluate.hpp
@@ -1,8 +1,8 @@
 #ifndef _NESO_PARTICLES_EXTERNAL_PETSC_DMPLEX_PROJECT_EVALUATE_H_
 #define _NESO_PARTICLES_EXTERNAL_PETSC_DMPLEX_PROJECT_EVALUATE_H_
 
-#include "../common/quadrature_point_mapper.hpp"
-#include "../petsc/dmplex_interface.hpp"
+#include "project_evaluate/dmplex_project_evaluate_base.hpp"
+#include "project_evaluate/dmplex_project_evaluate_dg.hpp"
 
 namespace NESO::Particles::PetscInterface {
 
@@ -11,83 +11,18 @@ namespace NESO::Particles::PetscInterface {
  */
 class DMPlexProjectEvaluate {
 protected:
-  std::shared_ptr<CellDatConst<REAL>> cdc_project;
-  std::shared_ptr<CellDatConst<REAL>> cdc_volumes;
-  DMPlexInterfaceSharedPtr mesh;
-
   inline void check_setup() {
     NESOASSERT(this->qpm->points_added(),
                "QuadraturePointMapper needs points adding to it.");
   }
 
-  inline void project_dg_0(ParticleGroupSharedPtr particle_group,
-                           Sym<REAL> sym) {
-    auto dat = particle_group->get_dat(sym);
-    const int ncomp = dat->ncomp;
-    auto destination_dat = this->qpm->get_sym(ncomp);
-
-    // DG0 projection onto the CellDatConst
-    this->cdc_project->fill(0.0);
-    particle_loop(
-        "DMPlexProjectEvaluate::project_0", particle_group,
-        [=](auto SRC, auto DST) {
-          for (int cx = 0; cx < ncomp; cx++) {
-            DST.fetch_add(cx, 0, SRC.at(cx));
-          }
-        },
-        Access::read(sym), Access::add(this->cdc_project))
-        ->execute();
-
-    // Read the CellDatConst values onto the quadrature point values
-    particle_loop(
-        "DMPlexProjectEvaluate::project_1", this->qpm->particle_group,
-        [=](auto VOLS, auto SRC, auto DST) {
-          const REAL iv = 1.0 / VOLS.at(0, 0);
-          for (int cx = 0; cx < ncomp; cx++) {
-            DST.at(cx) = SRC.at(cx, 0) * iv;
-          }
-        },
-        Access::read(this->cdc_volumes), Access::read(this->cdc_project),
-        Access::write(destination_dat))
-        ->execute();
-  }
-
-  inline void evaluate_dg_0(ParticleGroupSharedPtr particle_group,
-                            Sym<REAL> sym) {
-    auto dat = particle_group->get_dat(sym);
-    const int ncomp = dat->ncomp;
-    auto source_dat = this->qpm->get_sym(ncomp);
-
-    // Copy from the quadrature point values into the CellDatConst
-    particle_loop(
-        "DMPlexProjectEvaluate::evaluate_0", this->qpm->particle_group,
-        [=](auto SRC, auto DST, auto MASK) {
-          if (MASK.at(3) == 0) {
-            for (int cx = 0; cx < ncomp; cx++) {
-              DST.at(cx, 0) = SRC.at(cx);
-            }
-          }
-        },
-        Access::read(source_dat), Access::write(this->cdc_project),
-        Access::read(Sym<INT>("ADDING_RANK_INDEX")))
-        ->execute();
-
-    // Copy from the CellDatConst to the output particle group
-    particle_loop(
-        "DMPlexProjectEvaluate::evaluate_1", particle_group,
-        [=](auto SRC, auto DST) {
-          for (int cx = 0; cx < ncomp; cx++) {
-            DST.at(cx) = SRC.at(cx, 0);
-          }
-        },
-        Access::read(this->cdc_project), Access::write(sym))
-        ->execute();
-  }
-
 public:
+  DMPlexInterfaceSharedPtr mesh;
   ExternalCommon::QuadraturePointMapperSharedPtr qpm;
   std::string function_space;
   int polynomial_order;
+  std::shared_ptr<DMPlexProjectEvaluateBase> implementation;
+
   DMPlexProjectEvaluate() = default;
 
   /**
@@ -97,28 +32,32 @@ public:
                         std::string function_space, int polynomial_order)
       : qpm(qpm), function_space(function_space),
         polynomial_order(polynomial_order) {
+
+    std::map<std::string, std::pair<int, int>> map_allowed;
+    map_allowed["DG"] = {0, 0};
+    // map_allowed["Barycentric"] = {1, 1};
+
     NESOASSERT(this->qpm != nullptr, "QuadraturePointMapper is nullptr");
-    NESOASSERT(function_space == "DG",
-               "Only DG function spaces currently supported");
-    NESOASSERT(polynomial_order == 0,
-               "Only order 0 function spaces currently supported");
+    NESOASSERT(map_allowed.count(function_space),
+               "Only function space: " + function_space + " not recognised.");
+    const int p_min = map_allowed.at(function_space).first;
+    const int p_max = map_allowed.at(function_space).second;
+    NESOASSERT(((p_min <= polynomial_order) && (polynomial_order <= p_max)),
+               "Polynomial order " + std::to_string(polynomial_order) +
+                   " outside of acceptable range [" + std::to_string(p_min) +
+                   ", " + std::to_string(p_max) + "] for function space " +
+                   function_space + ".");
 
     this->mesh = std::dynamic_pointer_cast<PetscInterface::DMPlexInterface>(
         this->qpm->domain->mesh);
     NESOASSERT(this->mesh != nullptr,
                "Mesh is not descendent from PetscInterface::DMPlexInterface");
-    NESOASSERT(this->mesh->get_ndim() == 2, "Only implemented for 2D domains.");
 
-    const int cell_count = this->qpm->domain->mesh->get_cell_count();
-    this->cdc_project = std::make_shared<CellDatConst<REAL>>(
-        this->qpm->sycl_target, cell_count, 1, 1);
-    this->cdc_volumes = std::make_shared<CellDatConst<REAL>>(
-        this->qpm->sycl_target, cell_count, 1, 1);
-
-    // For each cell record the volume.
-    for (int cx = 0; cx < cell_count; cx++) {
-      const auto volume = this->mesh->dmh->get_cell_volume(cx);
-      this->cdc_volumes->set_value(cx, 0, 0, volume);
+    if (function_space == "DG") {
+      this->implementation =
+          std::dynamic_pointer_cast<DMPlexProjectEvaluateBase>(
+              std::make_shared<DMPlexProjectEvaluateDG>(qpm, function_space,
+                                                        polynomial_order));
     }
   }
 
@@ -131,7 +70,7 @@ public:
     this->check_setup();
     NESOASSERT(particle_group->contains_dat(sym),
                "ParticleGroup does not contain the sym: " + sym.name);
-    this->project_dg_0(particle_group, sym);
+    this->implementation->project(particle_group, sym);
   }
 
   /**
@@ -143,7 +82,7 @@ public:
     this->check_setup();
     NESOASSERT(particle_group->contains_dat(sym),
                "ParticleGroup does not contain the sym: " + sym.name);
-    this->evaluate_dg_0(particle_group, sym);
+    this->implementation->evaluate(particle_group, sym);
   }
 };
 
---
+++ include/neso_particles/external_interfaces/petsc/project_evaluate/dmplex_project_evaluate_base.hpp
@@ -0,0 +1,31 @@
+#ifndef _NESO_PARTICLES_EXTERNAL_PETSC_DMPLEX_PROJECT_EVALUATE_BASE_H_
+#define _NESO_PARTICLES_EXTERNAL_PETSC_DMPLEX_PROJECT_EVALUATE_BASE_H_
+
+#include "../../common/quadrature_point_mapper.hpp"
+#include "../../petsc/dmplex_interface.hpp"
+
+namespace NESO::Particles::PetscInterface {
+
+class DMPlexProjectEvaluateBase {
+protected:
+public:
+  /**
+   * TODO
+   * Projects values from particle data onto the values in the
+   * QuadraturePointMapper.
+   */
+  virtual inline void project(ParticleGroupSharedPtr particle_group,
+                              Sym<REAL> sym) = 0;
+
+  /**
+   * TODO
+   * Evaluates the function defined by the values in the QuadraturePointValues
+   * at the particle locations.
+   */
+  virtual inline void evaluate(ParticleGroupSharedPtr particle_group,
+                               Sym<REAL> sym) = 0;
+};
+
+} // namespace NESO::Particles::PetscInterface
+
+#endif
