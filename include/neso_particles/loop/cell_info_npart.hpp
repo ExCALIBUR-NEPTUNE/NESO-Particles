1ac91649bca261326cbde64cef18c791e14ae786
Will Saunders <will.saunders@ukaea.uk>
2025-03-07 12:53:41 +0000

    initial implementation of CellInfoNPart for accessing npart cell inside kernels efficiently

---
+++ include/neso_particles/loop/cell_info_npart.hpp
@@ -0,0 +1,83 @@
+#ifndef _NESO_PARTICLES_PARTICLE_LOOP_CELL_INFO_NPART_HPP_
+#define _NESO_PARTICLES_PARTICLE_LOOP_CELL_INFO_NPART_HPP_
+
+#include "particle_loop_base.hpp"
+
+namespace NESO::Particles {
+
+/**
+ * The type to pass to a ParticleLoop with read access to access the number of
+ * particles in the cell for the iteration set passed to the ParticleLoop.
+ */
+struct CellInfoNPart {};
+
+/**
+ * Namespace for the kernel types of CellInfoNPart.
+ */
+namespace Access::CellInfoNPart {
+
+/**
+ * Kernel type for read access.
+ */
+struct Read {
+  /// Number of particles in the cell (intended for internal use).
+  INT npart_cell;
+
+  /**
+   * @returns The number of particles in the cell for the iteration set of the
+   * ParticleLoop.
+   */
+  inline INT get() const { return this->npart_cell; }
+};
+
+} // namespace Access::CellInfoNPart
+
+namespace ParticleLoopImplementation {
+
+/**
+ * Device copyable host loop type for CellInfoNPart.
+ */
+struct CellInfoNPartKernelT {
+  int const *d_npart_cell;
+};
+
+/**
+ *  KernelParameter type for read-only access to a CellInfoNPart.
+ */
+template <> struct KernelParameter<Access::Read<CellInfoNPart>> {
+  using type = Access::CellInfoNPart::Read;
+};
+
+/**
+ *  Loop parameter for read access of a CellInfoNPart.
+ */
+template <> struct LoopParameter<Access::Read<CellInfoNPart>> {
+  using type = CellInfoNPartKernelT;
+};
+
+/**
+ * Method to compute access to a CellInfoNPart (read)
+ */
+inline CellInfoNPartKernelT
+create_loop_arg(ParticleLoopImplementation::ParticleLoopGlobalInfo *global_info,
+                [[maybe_unused]] sycl::handler &cgh,
+                [[maybe_unused]] Access::Read<CellInfoNPart *> &a) {
+  NESOASSERT(global_info->loop_type_int == 0 || global_info->loop_type_int == 1,
+             "Unknown loop type for CellInfoNPart.");
+  return {global_info->d_npart_cell};
+}
+
+/**
+ *  Function to create the kernel argument for CellInfoNPart read access.
+ */
+inline void create_kernel_arg(ParticleLoopIteration &iterationx,
+                              CellInfoNPartKernelT &rhs,
+                              Access::CellInfoNPart::Read &lhs) {
+  lhs.npart_cell = static_cast<INT>(rhs.d_npart_cell[iterationx.cellx]);
+}
+
+} // namespace ParticleLoopImplementation
+
+} // namespace NESO::Particles
+
+#endif
---
+++ test/test_particle_loop_cell_info.cpp
@@ -0,0 +1,109 @@
+#include "include/test_neso_particles.hpp"
+
+namespace {
+
+const int ndim = 2;
+
+ParticleGroupSharedPtr particle_loop_common(const int N = 4093) {
+  std::vector<int> dims(ndim);
+  dims[0] = 4;
+  dims[1] = 8;
+
+  const double cell_extent = 1.0;
+  const int subdivision_order = 2;
+
+  auto mesh = std::make_shared<CartesianHMesh>(MPI_COMM_WORLD, ndim, dims,
+                                               cell_extent, subdivision_order);
+
+  auto sycl_target =
+      std::make_shared<SYCLTarget>(GPU_SELECTOR, mesh->get_comm());
+
+  auto cart_local_mapper = CartesianHMeshLocalMapper(sycl_target, mesh);
+  auto domain = std::make_shared<Domain>(mesh, cart_local_mapper);
+
+  ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
+                             ParticleProp(Sym<REAL>("V"), 3),
+                             ParticleProp(Sym<REAL>("P2"), ndim),
+                             ParticleProp(Sym<INT>("CELL_ID"), 1, true),
+                             ParticleProp(Sym<INT>("LOOP_INDEX"), 2),
+                             ParticleProp(Sym<INT>("ID"), 1)};
+
+  auto A = std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
+  A->add_particle_dat(ParticleDat(sycl_target,
+                                  ParticleProp(Sym<REAL>("FOO"), 3),
+                                  domain->mesh->get_cell_count()));
+
+  const int rank = sycl_target->comm_pair.rank_parent;
+  const INT id_offset = rank * N;
+
+  std::mt19937 rng_pos(52234234 + rank);
+  std::mt19937 rng_vel(52234231 + rank);
+
+  auto positions =
+      uniform_within_extents(N, ndim, mesh->global_extents, rng_pos);
+  auto velocities =
+      NESO::Particles::normal_distribution(N, 3, 0.0, 1.0, rng_vel);
+
+  ParticleSet initial_distribution(N, particle_spec);
+
+  for (int px = 0; px < N; px++) {
+    for (int dimx = 0; dimx < ndim; dimx++) {
+      initial_distribution[Sym<REAL>("P")][px][dimx] = positions[dimx][px];
+    }
+    for (int dimx = 0; dimx < 3; dimx++) {
+      initial_distribution[Sym<REAL>("V")][px][dimx] = velocities[dimx][px];
+    }
+    initial_distribution[Sym<INT>("CELL_ID")][px][0] = 0;
+    initial_distribution[Sym<INT>("ID")][px][0] = px + id_offset;
+  }
+
+  A->add_particles_local(initial_distribution);
+  parallel_advection_initialisation(A, 16);
+
+  auto ccb = std::make_shared<CartesianCellBin>(
+      sycl_target, mesh, A->position_dat, A->cell_id_dat);
+
+  ccb->execute();
+  A->cell_move();
+
+  return A;
+}
+
+} // namespace
+
+TEST(ParticleLoop, cell_info_npart) {
+  auto A = particle_loop_common();
+  auto domain = A->domain;
+  auto mesh = domain->mesh;
+  auto sycl_target = A->sycl_target;
+  const int cell_count = mesh->get_cell_count();
+
+  std::vector<int> correct(cell_count);
+  for (int cx = 0; cx < cell_count; cx++) {
+    correct.at(cx) = A->get_npart_cell(cx);
+  }
+
+  auto la_to_test = std::make_shared<LocalArray<int>>(sycl_target, cell_count);
+  la_to_test->fill(0);
+
+  particle_loop(
+      A,
+      [=](auto INDEX, auto CELL_INFO_NPART, auto LA) {
+        // Is this the first particle in the cell?
+        if (INDEX.loop_layer == 0) {
+          LA.at(INDEX.cell) = CELL_INFO_NPART.get();
+        }
+      },
+      Access::read(ParticleLoopIndex{}), Access::read(CellInfoNPart{}),
+      Access::write(la_to_test))
+      ->execute();
+
+  auto h_to_test = la_to_test->get();
+  for (int cx = 0; cx < cell_count; cx++) {
+    EXPECT_EQ(h_to_test.at(cx), correct.at(cx));
+  }
+
+  A->free();
+  sycl_target->free();
+  mesh->free();
+}
