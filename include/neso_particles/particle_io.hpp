06702c153b8d6e546e503ee3f39d0772603ed2f5
Will Saunders <will.saunders@ukaea.uk>
2025-04-09 15:41:06 +0100

    refactor to enable writing h5part from sub groups passes existing tests

--- include/neso_particles/particle_io.hpp
+++ include/neso_particles/particle_io.hpp
@@ -1,8 +1,10 @@
 #ifndef _NESO_PARTICLES_PARTICLE_IO
 #define _NESO_PARTICLES_PARTICLE_IO
 
+#include "loop/particle_loop.hpp"
 #include "particle_group.hpp"
 #include "particle_spec.hpp"
+#include "particle_sub_group/particle_sub_group.hpp"
 #include "typedefs.hpp"
 #include <algorithm>
 #include <cstdint>
@@ -23,7 +25,9 @@ private:
   hid_t plist_id, file_id;
   INT step;
   ParticleGroupSharedPtr particle_group;
-  std::string position_labels[3] = {"x", "y", "z"};
+  ParticleSubGroupSharedPtr particle_sub_group;
+  static constexpr char position_labels[][3] = {"x", "y", "z"};
+
   bool multi_dim_mode = false;
 
   inline size_t get_max_particle_size() {
@@ -56,44 +60,56 @@ private:
 
   // Get the HDF5 type that matches the datatype the particle data was cast to
   // when linearised on the host
-  inline hid_t memtypeid(ParticleDatSharedPtr<REAL>) {
+  static inline hid_t memtypeid(ParticleDatSharedPtr<REAL>) {
     return H5T_NATIVE_DOUBLE;
   }
-  inline hid_t memtypeid(ParticleDatSharedPtr<INT>) { return H5T_NATIVE_LLONG; }
+  static inline hid_t memtypeid(ParticleDatSharedPtr<INT>) {
+    return H5T_NATIVE_LLONG;
+  }
 
   /**
    *  Write a ParticleDat to the HDF5 file where each component has its own
    *  entry (preferred by Paraview).
    */
-  template <typename T>
-  inline void write_dat_column_wise(const int64_t npart_local,
-                                    BufferHost<char> &pack_buffer,
-                                    ParticleDatSharedPtr<T> dat, hid_t dxpl,
-                                    hid_t group_step, hid_t memspace,
-                                    hid_t filespace, bool is_position) {
+  template <typename GROUP_TYPE, typename T>
+  static inline void
+  write_dat_column_wise(std::shared_ptr<GROUP_TYPE> parent,
+                        const int64_t npart_local, ParticleDatSharedPtr<T> dat,
+                        hid_t dxpl, hid_t group_step, hid_t memspace,
+                        hid_t filespace, bool is_position) {
+
+    auto sycl_target = get_particle_group(parent)->sycl_target;
     const int ncomp = dat->ncomp;
-    const int cell_count = dat->ncell;
-    const size_t component_stride =
-        static_cast<size_t>(npart_local) * sizeof(T);
+    NESOASSERT(npart_local == parent->get_npart_local(),
+               "Missmatch in npart local.");
 
-    // Get the data on the host
-    size_t pack_offset = 0;
-    for (int cellx = 0; cellx < cell_count; cellx++) {
-      auto cell_dat = dat->cell_dat.get_cell(cellx);
+    auto dh_buffer = get_resource<BufferDeviceHost<T>,
+                                  ResourceStackInterfaceBufferDeviceHost<T>>(
+        sycl_target->resource_stack_map, ResourceStackKeyBufferDeviceHost<T>{},
+        sycl_target);
+    dh_buffer->realloc_no_copy(ncomp * npart_local);
 
-      for (int cx = 0; cx < ncomp; cx++) {
-        this->memcpy_dat(pack_buffer.ptr + cx * component_stride + pack_offset,
-                         cell_dat->get_column_ptr(cx), cell_dat->nrow);
-      }
-      pack_offset += sizeof(T) * cell_dat->nrow;
-    }
+    auto k_ptr = dh_buffer->d_buffer.ptr;
+
+    particle_loop(
+        "H5Part::write_dat_column_wise", parent,
+        [=](auto INDEX, auto DAT) {
+          const auto dst_index = INDEX.get_loop_linear_index();
+          for (int cx = 0; cx < ncomp; cx++) {
+            k_ptr[cx * npart_local + dst_index] = DAT.at(cx);
+          }
+        },
+        Access::read(ParticleLoopIndex{}), Access::read(dat->sym))
+        ->execute();
+
+    dh_buffer->device_to_host();
 
     // write each component as a separate array in the HDF5 file.
     for (int cx = 0; cx < ncomp; cx++) {
 
       std::string name;
       if (is_position) {
-        name = this->position_labels[cx];
+        name = position_labels[cx];
       } else {
         name = dat->name + "_" + std::to_string(cx);
       }
@@ -104,38 +120,50 @@ private:
       hid_t dset = H5Dcreate2(group_step, name.c_str(), memtypeid(dat),
                               filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
       H5CHK(H5Dwrite(dset, memtypeid(dat), memspace, filespace, dxpl,
-                     pack_buffer.ptr + component_stride * cx));
+                     dh_buffer->h_buffer.ptr + npart_local * cx));
 
       H5CHK(H5Dclose(dset));
     }
+
+    restore_resource(sycl_target->resource_stack_map,
+                     ResourceStackKeyBufferDeviceHost<T>{}, dh_buffer);
   }
 
   /**
    *  Write a ParticleDat to the HDF5 file where each dat is stored as a 2D
    *  array.
    */
-  template <typename T>
-  inline void write_dat_2d(const int64_t npart_total, const int64_t npart_local,
-                           const int64_t offset, BufferHost<char> &pack_buffer,
-                           ParticleDatSharedPtr<T> dat, hid_t group_step,
-                           hid_t dxpl) {
+  template <typename GROUP_TYPE, typename T>
+  inline void write_dat_2d(std::shared_ptr<GROUP_TYPE> parent,
+                           const int64_t npart_total, const int64_t npart_local,
+                           const int64_t offset, ParticleDatSharedPtr<T> dat,
+                           hid_t group_step, hid_t dxpl) {
+
+    auto sycl_target = get_particle_group(parent)->sycl_target;
     const int ncomp = dat->ncomp;
-    const int cell_count = dat->ncell;
+    NESOASSERT(npart_local == parent->get_npart_local(),
+               "Missmatch in npart local.");
 
-    // Linearise the data on the host.
-    INT pack_offset = 0;
-    for (int cellx = 0; cellx < cell_count; cellx++) {
-      auto cell_dat = dat->cell_dat.get_cell(cellx);
+    auto dh_buffer = get_resource<BufferDeviceHost<T>,
+                                  ResourceStackInterfaceBufferDeviceHost<T>>(
+        sycl_target->resource_stack_map, ResourceStackKeyBufferDeviceHost<T>{},
+        sycl_target);
+    dh_buffer->realloc_no_copy(ncomp * npart_local);
 
-      for (int rowx = 0; rowx < cell_dat->nrow; rowx++) {
-        for (int colx = 0; colx < cell_dat->ncol; colx++) {
-          this->memcpy_dat(pack_buffer.ptr +
-                               (pack_offset + rowx * ncomp + colx) * sizeof(T),
-                           cell_dat->get_column_ptr(colx) + rowx, 1);
-        }
-      }
-      pack_offset += cell_dat->nrow * cell_dat->ncol;
-    }
+    auto k_ptr = dh_buffer->d_buffer.ptr;
+
+    particle_loop(
+        "H5Part::write_dat_column_wise", parent,
+        [=](auto INDEX, auto DAT) {
+          const auto dst_index = INDEX.get_loop_linear_index();
+          for (int cx = 0; cx < ncomp; cx++) {
+            k_ptr[cx * npart_local + dst_index] = DAT.at(cx);
+          }
+        },
+        Access::read(ParticleLoopIndex{}), Access::read(dat->sym))
+        ->execute();
+
+    dh_buffer->device_to_host();
 
     // create the memspace and filespace for the 2D array
     hsize_t dims_memspace[2] = {static_cast<hsize_t>(npart_local),
@@ -161,12 +189,15 @@ private:
                             H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
     // write the 2D array
     H5CHK(H5Dwrite(dset, memtypeid(dat), memspace, filespace, dxpl,
-                   pack_buffer.ptr));
+                   dh_buffer->h_buffer.ptr));
 
     H5CHK(H5Dclose(dset));
 
     H5CHK(H5Sclose(filespace));
     H5CHK(H5Sclose(memspace));
+
+    restore_resource(sycl_target->resource_stack_map,
+                     ResourceStackKeyBufferDeviceHost<T>{}, dh_buffer);
   }
 
   /**
@@ -187,7 +218,7 @@ private:
   /**
    * Open the file if it is closed.
    */
-  inline void open() {
+  inline void open_read_write() {
     if (this->is_closed) {
       this->plist_id = H5Pcreate(H5P_FILE_ACCESS);
       H5CHK(H5Pset_fapl_mpio(this->plist_id, this->comm_pair.comm_parent,
@@ -198,73 +229,17 @@ private:
     }
   }
 
-public:
-  /// Disable (implicit) copies.
-  H5Part(const H5Part &st) = delete;
-  /// Disable (implicit) copies.
-  H5Part &operator=(H5Part const &a) = delete;
-
-  ~H5Part() {
-    NESOASSERT(this->is_closed, "H5Part file was not closed correctly.");
-  };
-
-  /**
-   *  Construct a H5Part writer for a given set of ParticleDats described by
-   *  Sym<type>(name) instances. Must be called collectively on the
-   * communicator.
-   *
-   *  @param filename Output filename, e.g. "foo.h5part".
-   *  @param particle_group ParticleGroupSharedPtr instance.
-   *  @param args Remaining arguments (variable length) should be sym instances
-   *  indicating which ParticleDats are to be written.
-   */
-  template <typename... T>
-  H5Part(std::string filename, ParticleGroupSharedPtr particle_group,
-         T &&...args)
-      : filename(filename), comm_pair(particle_group->sycl_target->comm_pair),
-        sym_store(std::forward<T>(args)...), particle_group(particle_group),
-        multi_dim_mode(false) {
-    this->plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5CHK(H5Pset_fapl_mpio(this->plist_id, this->comm_pair.comm_parent,
-                           MPI_INFO_NULL));
-    this->file_id = H5Fcreate(this->filename.c_str(), H5F_ACC_TRUNC,
-                              H5P_DEFAULT, this->plist_id);
-    this->is_closed = false;
-    this->step = 0;
-  }
-
-  /**
-   *  Close the H5Part writer. Must be called before execution completes. Must
-   *  be called collectively on the communicator. Can optionally be called
-   *  after calling write to close the file such that if the simulation errors
-   *  the particle trajectory is readable.
-   */
-  inline void close() {
-    if (!this->is_closed) {
-      H5CHK(H5Fclose(this->file_id));
-      H5CHK(H5Pclose(this->plist_id));
-    }
-    this->is_closed = true;
-  };
-
   /**
    *  Write ParticleDats as 2D arrays in the HDF5 file.
+   *  TODO This needs better testing if anyone wants it and for it to be made
+   * public.
    */
   inline void enable_multi_dim_mode() { this->multi_dim_mode = true; }
 
-  /**
-   * Write the current particle data to the HDF5 file as a new time step. Must
-   * be called collectively on the communicator. Will open the file if required.
-   *
-   * @param step_in Optionally set the step explicitly.
-   */
-  inline void write(INT step_in = -1) {
-    // open the file for writing if required.
-    this->open();
-
-    if (step_in >= 0) {
-      this->step = step_in;
-    }
+  template <typename GROUP_TYPE>
+  inline void write_inner(std::shared_ptr<GROUP_TYPE> group) {
+    auto particle_group = get_particle_group(group);
+    auto sycl_target = particle_group->sycl_target;
 
     // check the ParticleDats actually exist
     for (auto &sym : this->sym_store.syms_real) {
@@ -281,8 +256,7 @@ public:
                                  H5P_DEFAULT, H5P_DEFAULT);
 
     // Perform the bookkeeping logic once for all ParticleDats
-    const int64_t npart_local =
-        this->particle_group->position_dat->get_npart_local();
+    const int64_t npart_local = group->get_npart_local();
     int64_t offset;
     MPICHK(MPI_Scan(&npart_local, &offset, 1, MPI_INT64_T, MPI_SUM,
                     this->comm_pair.comm_parent));
@@ -311,39 +285,33 @@ public:
       hid_t dxpl = H5Pcreate(H5P_DATASET_XFER);
       H5CHK(H5Pset_dxpl_mpio(dxpl, H5FD_MPIO_COLLECTIVE));
 
-      // create a host buffer in which to serialise the particle data before
-      // passing to HDF5
-      BufferHost<char> pack_buffer(this->particle_group->sycl_target,
-                                   npart_local * get_max_particle_size());
-
       // Write the positions explicitly in a way Paraview interprets as a H5Part
       // format.
-      write_dat_column_wise(npart_local, pack_buffer,
-                            this->particle_group->position_dat, dxpl,
-                            group_step, memspace, filespace, true);
+      write_dat_column_wise(group, npart_local, particle_group->position_dat,
+                            dxpl, group_step, memspace, filespace, true);
 
       // Write the particle data for each ParticleDat
       if (multi_dim_mode) {
         for (auto &sym : this->sym_store.syms_real) {
-          const auto dat = this->particle_group->get_dat(sym, false);
-          this->write_dat_2d(npart_total, npart_local, offset, pack_buffer, dat,
+          const auto dat = particle_group->get_dat(sym, false);
+          this->write_dat_2d(group, npart_total, npart_local, offset, dat,
                              group_step, dxpl);
         }
         for (auto &sym : this->sym_store.syms_int) {
-          const auto dat = this->particle_group->get_dat(sym, false);
-          this->write_dat_2d(npart_total, npart_local, offset, pack_buffer, dat,
+          const auto dat = particle_group->get_dat(sym, false);
+          this->write_dat_2d(group, npart_total, npart_local, offset, dat,
                              group_step, dxpl);
         }
       } else {
 
         for (auto &sym : this->sym_store.syms_real) {
-          const auto dat = (*this->particle_group)[sym];
-          write_dat_column_wise(npart_local, pack_buffer, dat, dxpl, group_step,
+          const auto dat = particle_group->get_dat(sym, false);
+          write_dat_column_wise(group, npart_local, dat, dxpl, group_step,
                                 memspace, filespace, false);
         }
         for (auto &sym : this->sym_store.syms_int) {
-          const auto dat = (*this->particle_group)[sym];
-          write_dat_column_wise(npart_local, pack_buffer, dat, dxpl, group_step,
+          const auto dat = particle_group->get_dat(sym, false);
+          write_dat_column_wise(group, npart_local, dat, dxpl, group_step,
                                 memspace, filespace, false);
         }
       }
@@ -353,6 +321,94 @@ public:
       H5CHK(H5Sclose(memspace));
     }
     H5CHK(H5Gclose(group_step));
+  }
+
+public:
+  /// Disable (implicit) copies.
+  H5Part(const H5Part &st) = delete;
+  /// Disable (implicit) copies.
+  H5Part &operator=(H5Part const &a) = delete;
+
+  ~H5Part() {
+    NESOASSERT(this->is_closed, "H5Part file was not closed correctly.");
+  };
+
+  /**
+   *  Construct a H5Part writer for a given set of ParticleDats described by
+   *  Sym<type>(name) instances. Must be called collectively on the
+   * communicator.
+   *
+   *  @param filename Output filename, e.g. "foo.h5part".
+   *  @param particle_group ParticleGroupSharedPtr instance.
+   *  @param args Remaining arguments (variable length) should be sym instances
+   *  indicating which ParticleDats are to be written.
+   */
+  template <typename... T>
+  H5Part(std::string filename, ParticleGroupSharedPtr particle_group,
+         T &&...args)
+      : filename(filename), comm_pair(particle_group->sycl_target->comm_pair),
+        sym_store(std::forward<T>(args)...), particle_group(particle_group),
+        particle_sub_group(nullptr), multi_dim_mode(false) {
+    this->plist_id = H5Pcreate(H5P_FILE_ACCESS);
+    H5CHK(H5Pset_fapl_mpio(this->plist_id, this->comm_pair.comm_parent,
+                           MPI_INFO_NULL));
+    this->file_id = H5Fcreate(this->filename.c_str(), H5F_ACC_TRUNC,
+                              H5P_DEFAULT, this->plist_id);
+    this->is_closed = false;
+    this->step = 0;
+  }
+
+  /**
+   *  Construct a H5Part writer for a given set of ParticleDats described by
+   *  Sym<type>(name) instances. Must be called collectively on the
+   * communicator.
+   *
+   *  @param filename Output filename, e.g. "foo.h5part".
+   *  @param particle_sub_group ParticleSubGroupSharedPtr instance.
+   *  @param args Remaining arguments (variable length) should be sym instances
+   *  indicating which ParticleDats are to be written.
+   */
+  template <typename... T>
+  H5Part(std::string filename, ParticleSubGroupSharedPtr particle_sub_group,
+         T &&...args)
+      : H5Part(filename, get_particle_group(particle_sub_group), args...) {
+    this->particle_sub_group = particle_sub_group;
+  }
+
+  /**
+   *  Close the H5Part writer. Must be called before execution completes. Must
+   *  be called collectively on the communicator. Can optionally be called
+   *  after calling write to close the file such that if the simulation errors
+   *  the particle trajectory is readable.
+   */
+  inline void close() {
+    if (!this->is_closed) {
+      H5CHK(H5Fclose(this->file_id));
+      H5CHK(H5Pclose(this->plist_id));
+    }
+    this->is_closed = true;
+  };
+
+  /**
+   * Write the current particle data to the HDF5 file as a new time step. Must
+   * be called collectively on the communicator. Will open the file if required.
+   *
+   * @param step_in Optionally set the step explicitly.
+   */
+  inline void write(INT step_in = -1) {
+    // open the file for writing if required.
+    this->open_read_write();
+
+    if (step_in >= 0) {
+      this->step = step_in;
+    }
+
+    if (this->particle_sub_group != nullptr) {
+      this->write_inner(this->particle_sub_group);
+    } else {
+      this->write_inner(this->particle_group);
+    }
+
     this->step++;
   };
 };
@@ -361,6 +417,11 @@ public:
 
 class H5Part {
 private:
+  /**
+   *  Write ParticleDats as 2D arrays in the HDF5 file.
+   */
+  inline void enable_multi_dim_mode() {}
+
 public:
   /// Disable (implicit) copies.
   H5Part(const H5Part &st) = delete;
@@ -382,15 +443,24 @@ public:
          T... args){};
 
   /**
+   *  Construct a H5Part writer for a given set of ParticleDats described by
+   *  Sym<type>(name) instances. Must be called collectively on the
+   * communicator.
+   *
+   *  @param filename Output filename, e.g. "foo.h5part".
+   *  @param particle_sub_group ParticleSubGroupSharedPtr instance.
+   *  @param args Remaining arguments (variable length) should be sym instances
+   *  indicating which ParticleDats are to be written.
+   */
+  template <typename... T>
+  H5Part(std::string filename, ParticleSubGroupSharedPtr particle_sub_group,
+         T... args){};
+
+  /**
    *  Close the H5Part writer. Must be called. Must be called collectively on
    *  the communicator.
    */
   inline void close() {};
-
-  /**
-   *  Write ParticleDats as 2D arrays in the HDF5 file.
-   */
-  inline void enable_multi_dim_mode() {}
 
   /**
    * Write the current particle data to the HDF5 file as a new time step. Must
