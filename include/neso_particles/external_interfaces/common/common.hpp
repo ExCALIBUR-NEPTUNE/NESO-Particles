e520cea283e2675f8c3f74ae0e8502a990b55524
Will Saunders <will.saunders@ukaea.uk>
2025-04-10 18:47:58 +0100

    hotfix missing header file includes

--- include/neso_particles/external_interfaces/common/common.hpp
+++ include/neso_particles/external_interfaces/common/common.hpp
@@ -1,8 +1,11 @@
 #ifndef _NESO_PARTICLES_EXTERNAL_INTERFACES_COMMON_COMMON_H_
 #define _NESO_PARTICLES_EXTERNAL_INTERFACES_COMMON_COMMON_H_
 
+#include "bounding_box.hpp"
 #include "coordinate_mapping.hpp"
 #include "dof_mapper_dg.hpp"
+#include "local_claim.hpp"
+#include "overlay_cartesian_mesh.hpp"
 #include "quadrature_point_mapper.hpp"
 
 #endif
