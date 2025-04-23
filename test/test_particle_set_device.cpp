c2685100758adab58af0b83c202afd3dc46fea2c
Will Saunders <will.saunders@ukaea.uk>
2025-04-23 11:12:41 +0100

    Basic ParticleLoop test for ParticleSetDevice.

--- test/test_particle_set_device.cpp
+++ test/test_particle_set_device.cpp
@@ -1,9 +1,8 @@
-#include <gtest/gtest.h>
-#include <neso_particles.hpp>
+#include "include/test_neso_particles.hpp"
 
 using namespace NESO::Particles;
 
-TEST(ParticleSetDevice, test_particle_set_device) {
+TEST(ParticleSetDevice, test_particle_set_device_base) {
 
   ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), 2, true),
                              ParticleProp(Sym<REAL>("V"), 3),
@@ -49,6 +48,92 @@ TEST(ParticleSetDevice, test_particle_set_device) {
                 set_host->at(Sym<INT>("ID"), px, 0));
     }
   }
+
+  sycl_target->free();
+}
+
+TEST(ParticleSetDevice, test_particle_set_device_loop) {
+  auto [A, sycl_target, cell_count_t] = particle_loop_common_2d(27, 16, 32);
+
+  ParticleSpec particle_spec{
+      ParticleProp(Sym<INT>("A"), 2),
+      ParticleProp(Sym<REAL>("B"), 3),
+      ParticleProp(Sym<INT>("C"), 1),
+      ParticleProp(Sym<REAL>("D"), 1),
+  };
+
+  const int npart_local = A->get_npart_local();
+  auto set_device = std::make_shared<ParticleSetDevice>(
+      sycl_target, npart_local, particle_spec);
+
+  particle_loop(
+      A,
+      [=](auto INDEX, auto PSD, auto V) {
+        PSD.at_int(INDEX.get_local_linear_index(), 0, 0) =
+            INDEX.get_local_linear_index();
+        PSD.at_int(INDEX.get_local_linear_index(), 0, 1) =
+            INDEX.get_local_linear_index() - 100;
+        for (int dx = 0; dx < 3; dx++) {
+          PSD.at_real(INDEX.get_local_linear_index(), 0, dx) = V.at(dx);
+        }
+      },
+      Access::read(ParticleLoopIndex{}), Access::write(set_device),
+      Access::read(Sym<REAL>("V")))
+      ->execute();
+
+  particle_loop(
+      A,
+      [=](auto PSD) {
+        for (int nx = 0; nx < npart_local; nx++) {
+          PSD.fetch_add_int(nx, 1, 0, 1);
+          PSD.fetch_add_real(nx, 1, 0, 1.1);
+        }
+      },
+      Access::add(set_device))
+      ->execute();
+
+  ErrorPropagate ep(sycl_target);
+  auto k_ep = ep.device_ptr();
+
+  ASSERT_EQ(A->get_dat(Sym<REAL>("V"))->ncomp, 3);
+
+  particle_loop(
+      A,
+      [=](auto INDEX, auto PSD, auto V) {
+        NESO_KERNEL_ASSERT(PSD.at_int(INDEX.get_local_linear_index(), 0, 0) ==
+                               INDEX.get_local_linear_index(),
+                           k_ep);
+        NESO_KERNEL_ASSERT(PSD.at_int(INDEX.get_local_linear_index(), 0, 1) ==
+                               INDEX.get_local_linear_index() - 100,
+                           k_ep);
+        for (int dx = 0; dx < 3; dx++) {
+          NESO_KERNEL_ASSERT(
+              PSD.at_real(INDEX.get_local_linear_index(), 0, dx) == V.at(dx),
+              k_ep);
+        }
+      },
+      Access::read(ParticleLoopIndex{}), Access::read(set_device),
+      Access::read(Sym<REAL>("V")))
+      ->execute();
+
+  ASSERT_FALSE(ep.get_flag());
+
+  particle_loop(
+      A,
+      [=](auto INDEX, auto PSD) {
+        NESO_KERNEL_ASSERT(PSD.at_int(INDEX.get_local_linear_index(), 1, 0) ==
+                               npart_local,
+                           k_ep);
+        NESO_KERNEL_ASSERT(
+            Kernel::abs(PSD.at_real(INDEX.get_local_linear_index(), 1, 0) -
+                        npart_local * 1.1) /
+                    (npart_local * 1.1) <
+                1.0e-10,
+            k_ep);
+      },
+      Access::read(ParticleLoopIndex{}), Access::read(set_device))
+      ->execute();
+  ASSERT_FALSE(ep.get_flag());
 
   sycl_target->free();
 }
