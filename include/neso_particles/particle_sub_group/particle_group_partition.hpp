b5732b93e228dbef762ea98e9dd33ae25a1a74d8
Will Saunders <will.saunders@ukaea.uk>
2025-05-23 14:35:21 +0100

    Split the DMPlex boundary tests into more files.

--- include/neso_particles/particle_sub_group/particle_group_partition.hpp
+++ include/neso_particles/particle_sub_group/particle_group_partition.hpp
@@ -47,21 +47,11 @@ public:
   std::function<void(Selection *created_selection)> create_handle;
   std::function<void()> destroy_handle;
 
-  virtual inline void create(Selection *created_selection) override {
-    this->create_handle(created_selection);
-  }
-
-  inline std::shared_ptr<SubGroupParticleMap> get_sub_group_particle_map() {
-    return this->sub_group_particle_map;
-  }
-
-  inline void set_create_handle(
-      std::function<void(Selection *created_selection)> create_handle) {
-    this->create_handle = create_handle;
-  }
-  inline void set_destroy_handle(std::function<void()> destroy_handle) {
-    this->destroy_handle = destroy_handle;
-  }
+  virtual void create(Selection *created_selection) override;
+  std::shared_ptr<SubGroupParticleMap> get_sub_group_particle_map();
+  void set_create_handle(
+      std::function<void(Selection *created_selection)> create_handle);
+  void set_destroy_handle(std::function<void()> destroy_handle);
 
 public:
   virtual ~ParticleGroupPartitionSelector() override { this->destroy_handle(); }
@@ -103,15 +93,7 @@ public:
 
   ParticleLoopSharedPtr loop_0, loop_1;
 
-  inline void create_indexed(const std::size_t index,
-                             Selection *created_selection) {
-    NESOASSERT(index < this->num_partitions, "Bad index passed.");
-
-    // This is where the versions get checked and the internal representation
-    // for all the partitions gets updated.
-    this->get(nullptr);
-    *created_selection = this->partition_selections.at(index);
-  }
+  void create_indexed(const std::size_t index, Selection *created_selection);
 
   /**
    * This is the overrided method which is called by get to create all the
@@ -119,175 +101,7 @@ public:
    * ParticleGroupPartitionSelector will extract its selection from the vector
    * of selections.
    */
-  virtual inline void create(Selection *created_selection) override {
-    if (this->num_partitions == 0) {
-      return;
-    }
-
-    NESOASSERT(created_selection == nullptr, "Expected a nullptr.");
-    auto sycl_target = this->particle_group->sycl_target;
-    const std::size_t cell_count = static_cast<std::size_t>(
-        this->particle_group->domain->mesh->get_cell_count());
-
-    // Find once at the begining of this function which of the selectors
-    // actually still exist and have not been deleted.
-    std::vector<int> still_exists(this->num_partitions);
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      still_exists[px] =
-          static_cast<int>(!this->sub_group_particle_maps.at(px).expired());
-    }
-    this->la_still_exists->set(still_exists);
-
-    // Create overall map.
-    auto d_cell_counts = get_resource<BufferDevice<int>,
-                                      ResourceStackInterfaceBufferDevice<int>>(
-        sycl_target->resource_stack_map, ResourceStackKeyBufferDevice<int>{},
-        sycl_target);
-
-    auto d_cell_counts_INT =
-        get_resource<BufferDevice<INT>,
-                     ResourceStackInterfaceBufferDevice<INT>>(
-            sycl_target->resource_stack_map,
-            ResourceStackKeyBufferDevice<INT>{}, sycl_target);
-    auto d_particle_layers =
-        get_resource<BufferDevice<int>,
-                     ResourceStackInterfaceBufferDevice<int>>(
-            sycl_target->resource_stack_map,
-            ResourceStackKeyBufferDevice<int>{}, sycl_target);
-
-    const std::size_t cell_count_all_partitions =
-        cell_count * this->num_partitions;
-    d_cell_counts->realloc_no_copy(cell_count_all_partitions);
-    d_particle_layers->realloc_no_copy(this->particle_group->get_npart_local());
-    d_cell_counts_INT->realloc_no_copy(cell_count_all_partitions);
-
-    int *k_cell_counts = d_cell_counts->ptr;
-    int *k_particle_layers = d_particle_layers->ptr;
-    INT *k_cell_counts_INT = d_cell_counts_INT->ptr;
-
-    sycl_target->queue
-        .fill(k_cell_counts, static_cast<int>(0), cell_count_all_partitions)
-        .wait_and_throw();
-
-    // Assemble the global particle maps.
-    std::vector<int *> tmp = {k_particle_layers, k_cell_counts};
-    this->map_ptrs->set(tmp);
-    this->loop_0->execute();
-    EventStack es;
-
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        int *k_cell_counts_int = this->sub_group_particle_maps.at(px)
-                                     .lock()
-                                     ->dh_npart_cell->d_buffer.ptr;
-        INT *k_cell_counts_es = this->sub_group_particle_maps.at(px)
-                                    .lock()
-                                    ->dh_npart_cell_es->d_buffer.ptr;
-        es.push(sycl_target->queue.parallel_for(
-            sycl::range<1>(cell_count), [=](auto idx) {
-              const int cell_count_inner = k_cell_counts[cell_count * px + idx];
-              k_cell_counts_int[idx] = cell_count_inner;
-              k_cell_counts_INT[cell_count * px + idx] =
-                  static_cast<INT>(cell_count_inner);
-              k_cell_counts_es[idx] = 0;
-            }));
-      }
-    }
-    es.wait();
-
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        INT *k_cell_counts_es = this->sub_group_particle_maps.at(px)
-                                    .lock()
-                                    ->dh_npart_cell_es->d_buffer.ptr;
-        es.push(joint_exclusive_scan(sycl_target, cell_count,
-                                     k_cell_counts_INT + px * cell_count,
-                                     k_cell_counts_es));
-      }
-    }
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        this->sub_group_particle_maps.at(px)
-            .lock()
-            ->dh_npart_cell->device_to_host();
-      }
-    }
-    es.wait();
-
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        this->sub_group_particle_maps.at(px)
-            .lock()
-            ->dh_npart_cell_es->device_to_host();
-      }
-    }
-
-    // Create the particle map for each partition.
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        int *h_cell_counts = this->sub_group_particle_maps.at(px)
-                                 .lock()
-                                 ->dh_npart_cell->h_buffer.ptr;
-        INT *h_cell_counts_es = this->sub_group_particle_maps.at(px)
-                                    .lock()
-                                    ->dh_npart_cell_es->h_buffer.ptr;
-        this->sub_group_particle_maps.at(px).lock()->create(
-            0, cell_count, h_cell_counts, h_cell_counts_es);
-      }
-    }
-
-    // Populate the particle maps from the particles
-    std::vector<INT *> h_layer_maps(this->num_partitions);
-    std::vector<INT *> h_npart_cell_es(this->num_partitions);
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-      if (still_exists[px]) {
-        h_layer_maps[px] =
-            this->sub_group_particle_maps.at(px).lock()->d_layer_map->ptr;
-        h_npart_cell_es[px] = this->sub_group_particle_maps.at(px)
-                                  .lock()
-                                  ->dh_npart_cell_es->d_buffer.ptr;
-      } else {
-        h_layer_maps[px] = nullptr;
-        h_npart_cell_es[px] = nullptr;
-      }
-    }
-    this->la_layer_maps->set(h_layer_maps);
-    this->la_npart_cell_es->set(h_npart_cell_es);
-    this->loop_1->submit();
-
-    // Create the selections from the particle maps.
-    for (std::size_t px = 0; px < this->num_partitions; px++) {
-
-      if (still_exists[px]) {
-        auto [h_npart_cell_ptr, d_npart_cell_ptr, h_npart_cell_es_ptr,
-              d_npart_cell_es_ptr] =
-            this->sub_group_particle_maps.at(px).lock()->get_helper_ptrs();
-
-        INT npart_total =
-            this->sub_group_particle_maps.at(px).lock()->npart_total;
-        auto d_cell_starts_ptr =
-            this->sub_group_particle_maps.at(px).lock()->d_cell_starts->ptr;
-
-        Selection s;
-        s.npart_local = npart_total;
-        s.ncell = cell_count;
-        s.h_npart_cell = h_npart_cell_ptr;
-        s.d_npart_cell = d_npart_cell_ptr;
-        s.d_npart_cell_es = d_npart_cell_es_ptr;
-        s.d_map_cells_to_particles = {d_cell_starts_ptr};
-        this->partition_selections.at(px) = s;
-      }
-    }
-
-    this->loop_1->wait();
-
-    restore_resource(sycl_target->resource_stack_map,
-                     ResourceStackKeyBufferDevice<int>{}, d_particle_layers);
-    restore_resource(sycl_target->resource_stack_map,
-                     ResourceStackKeyBufferDevice<INT>{}, d_cell_counts_INT);
-    restore_resource(sycl_target->resource_stack_map,
-                     ResourceStackKeyBufferDevice<int>{}, d_cell_counts);
-  }
+  void create(Selection *created_selection) override;
 
 public:
   /// The particle property used to partition the ParticleGroup.
@@ -312,6 +126,7 @@ public:
           &partition_selectors_in)
       : SubGroupSelectorBase(parent), partition_sym(partition_sym),
         num_partitions(num_partitions) {
+
     NESOASSERT(partition_selectors_in.size() == num_partitions,
                "size missmatch");
 
@@ -404,6 +219,18 @@ public:
   }
 };
 
+extern template ParticleGroupPartitioner::ParticleGroupPartitioner(
+    std::shared_ptr<ParticleGroup> parent, Sym<INT> partition_sym,
+    const std::size_t num_partitions,
+    std::vector<std::shared_ptr<ParticleGroupPartitionSelector>>
+        &partition_selectors_in);
+
+extern template ParticleGroupPartitioner::ParticleGroupPartitioner(
+    std::shared_ptr<ParticleSubGroup> parent, Sym<INT> partition_sym,
+    const std::size_t num_partitions,
+    std::vector<std::shared_ptr<ParticleGroupPartitionSelector>>
+        &partition_selectors_in);
+
 } // namespace NESO::Particles::ParticleSubGroupImplementation
 
 namespace NESO::Particles {
@@ -476,6 +303,14 @@ particle_group_partition(std::shared_ptr<PARENT> parent, Sym<INT> partition_sym,
   }
   return sub_groups;
 }
+
+extern template std::vector<ParticleSubGroupSharedPtr>
+particle_group_partition(std::shared_ptr<ParticleGroup> parent,
+                         Sym<INT> partition_sym, const int num_partitions);
+
+extern template std::vector<ParticleSubGroupSharedPtr>
+particle_group_partition(std::shared_ptr<ParticleSubGroup> parent,
+                         Sym<INT> partition_sym, const int num_partitions);
 
 } // namespace NESO::Particles
 
---
+++ test/test_petsc_boundary_interface_c.cpp
@@ -0,0 +1,216 @@
+#ifdef NESO_PARTICLES_PETSC
+
+#include "include/test_neso_particles.hpp"
+#include <neso_particles/external_interfaces/petsc/petsc_interface.hpp>
+
+namespace {
+class TestBoundaryInteraction2D : public PetscInterface::BoundaryInteraction2D {
+public:
+  TestBoundaryInteraction2D(
+      SYCLTargetSharedPtr sycl_target,
+      PetscInterface::DMPlexInterfaceSharedPtr mesh,
+      std::map<PetscInt, std::vector<PetscInt>> &boundary_groups,
+      const REAL tol = 0.0,
+      std::optional<Sym<REAL>> previous_position_sym = std::nullopt)
+      :
+
+        PetscInterface::BoundaryInteraction2D(
+            sycl_target, mesh, boundary_groups, tol, previous_position_sym) {}
+
+  using BoundaryInteractionCellData2D =
+      PetscInterface::BoundaryInteractionCellData2D;
+  using BoundaryInteractionNormalData2D =
+      PetscInterface::BoundaryInteractionNormalData2D;
+
+  MAKE_WRAP_METHOD(get_labels)
+  MAKE_WRAP_METHOD(get_bounding_box)
+  MAKE_WRAP_METHOD(collect_cells)
+  MAKE_GETTER_METHOD(facets_real)
+  MAKE_GETTER_METHOD(facets_int)
+  MAKE_GETTER_METHOD(num_facets_global)
+  MAKE_GETTER_METHOD(ncomp_int)
+  MAKE_GETTER_METHOD(ncomp_real)
+  MAKE_GETTER_METHOD(required_mh_cells)
+  MAKE_GETTER_METHOD(collected_mh_cells)
+  MAKE_GETTER_METHOD(d_map_edge_discovery)
+  MAKE_GETTER_METHOD(d_map_edge_normals)
+};
+
+ParticleGroupSharedPtr particle_loop_common(DM dm, const int N = 1093) {
+
+  const int ndim = 2;
+  auto mesh =
+      std::make_shared<PetscInterface::DMPlexInterface>(dm, 0, MPI_COMM_WORLD);
+
+  auto sycl_target =
+      std::make_shared<SYCLTarget>(GPU_SELECTOR, mesh->get_comm());
+
+  auto mapper =
+      std::make_shared<PetscInterface::DMPlexLocalMapper>(sycl_target, mesh);
+  auto domain = std::make_shared<Domain>(mesh, mapper);
+
+  ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
+                             ParticleProp(Sym<REAL>("V"), 3),
+                             ParticleProp(Sym<REAL>("U"), 3),
+                             ParticleProp(Sym<REAL>("TSP"), 2),
+                             ParticleProp(Sym<REAL>("P2"), ndim),
+                             ParticleProp(Sym<INT>("CELL_ID"), 1, true),
+                             ParticleProp(Sym<INT>("LOOP_INDEX"), 2),
+                             ParticleProp(Sym<INT>("ID"), 1)};
+
+  auto A = std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
+
+  int ncell_local = mesh->get_cell_count();
+  int ncell_global;
+
+  MPICHK(MPI_Allreduce(&ncell_local, &ncell_global, 1, MPI_INT, MPI_SUM,
+                       MPI_COMM_WORLD));
+  const int npart_per_cell = std::max(1, N / ncell_global);
+  const int rank = sycl_target->comm_pair.rank_parent;
+  const INT id_offset = rank * N;
+
+  std::mt19937 rng_pos(52234234 + rank);
+  std::mt19937 rng_vel(52234231 + rank);
+  std::vector<std::vector<double>> positions;
+  std::vector<int> cells;
+
+  uniform_within_dmplex_cells(mesh, npart_per_cell, positions, cells, &rng_pos);
+
+  const int N_actual = cells.size();
+  auto velocities =
+      NESO::Particles::normal_distribution(N_actual, 3, 0.0, 1.0, rng_vel);
+
+  ParticleSet initial_distribution(N_actual, particle_spec);
+
+  for (int px = 0; px < N_actual; px++) {
+    for (int dimx = 0; dimx < ndim; dimx++) {
+      initial_distribution[Sym<REAL>("P")][px][dimx] = positions[dimx][px];
+    }
+    for (int dimx = 0; dimx < 3; dimx++) {
+      initial_distribution[Sym<REAL>("V")][px][dimx] = velocities[dimx][px];
+    }
+    initial_distribution[Sym<INT>("CELL_ID")][px][0] = cells.at(px);
+    initial_distribution[Sym<INT>("ID")][px][0] = px + id_offset;
+  }
+
+  A->add_particles_local(initial_distribution);
+
+  return A;
+}
+
+} // namespace
+
+TEST(PETScBoundary2D, corners) {
+
+  PETSCCHK(PetscInitializeNoArguments());
+  DM dm;
+
+  const int ndim = 2;
+  const int mesh_size = 8;
+  const REAL h = 1.0;
+  PetscInt faces[3] = {mesh_size, mesh_size, mesh_size};
+  PetscReal lower[3] = {0.0, 0.0, 0.0};
+  PetscReal upper[3] = {mesh_size * h, mesh_size * h, mesh_size * h};
+
+  PETSCCHK(NPPETScAPI::NP_DMPlexCreateBoxMesh(
+      PETSC_COMM_WORLD, ndim, PETSC_FALSE, faces, lower, upper,
+      /* periodicity */ NULL, PETSC_TRUE, &dm));
+  PetscInterface::generic_distribute(&dm);
+  auto mesh =
+      std::make_shared<PetscInterface::DMPlexInterface>(dm, 0, MPI_COMM_WORLD);
+
+  auto sycl_target =
+      std::make_shared<SYCLTarget>(GPU_SELECTOR, mesh->get_comm());
+
+  auto mapper =
+      std::make_shared<PetscInterface::DMPlexLocalMapper>(sycl_target, mesh);
+  auto domain = std::make_shared<Domain>(mesh, mapper);
+
+  ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
+                             ParticleProp(Sym<REAL>("V"), 3),
+                             ParticleProp(Sym<REAL>("P2"), ndim),
+                             ParticleProp(Sym<INT>("CELL_ID"), 1, true),
+                             ParticleProp(Sym<INT>("LOOP_INDEX"), 2),
+                             ParticleProp(Sym<INT>("ID"), 1)};
+
+  auto A = std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
+
+  ParticleSet initial_distribution(1, particle_spec);
+  initial_distribution[Sym<REAL>("P")][0][0] = 0.5 * mesh_size * h;
+  initial_distribution[Sym<REAL>("P")][0][1] = 0.5 * mesh_size * h;
+  if (sycl_target->comm_pair.rank_parent == 0) {
+    A->add_particles_local(initial_distribution);
+  }
+  A->hybrid_move();
+  A->cell_move();
+
+  std::map<PetscInt, std::vector<PetscInt>> boundary_groups;
+  boundary_groups[1] = {1, 2, 3, 4};
+
+  auto b2d = std::make_shared<TestBoundaryInteraction2D>(sycl_target, mesh,
+                                                         boundary_groups);
+
+  std::vector<std::array<REAL, 4>> offsets;
+  offsets.push_back(
+      {(mesh_size + 1) * h, (mesh_size + 1) * h, mesh_size * h, mesh_size * h});
+  offsets.push_back({(mesh_size + 1) * h, -h, mesh_size * h, 0.0});
+  offsets.push_back({-h, (mesh_size + 1) * h, 0.0, mesh_size * h});
+  offsets.push_back({-h, -h, 0.0, 0.0});
+
+  auto reset_loop = particle_loop(
+      A,
+      [=](auto P) {
+        P.at(0) = mesh_size * 0.5 * h;
+        P.at(1) = mesh_size * 0.5 * h;
+      },
+      Access::write(Sym<REAL>("P")));
+
+  auto ep = std::make_shared<ErrorPropagate>(sycl_target);
+  auto k_ep = ep->device_ptr();
+
+  for (auto &ox : offsets) {
+    REAL finalx = ox.at(0);
+    REAL finaly = ox.at(1);
+    REAL intersectx = ox.at(2);
+    REAL intersecty = ox.at(3);
+
+    reset_loop->execute();
+    b2d->pre_integration(A);
+    particle_loop(
+        A,
+        [=](auto P) {
+          P.at(0) = finalx;
+          P.at(1) = finaly;
+        },
+        Access::write(Sym<REAL>("P")))
+        ->execute();
+    auto sub_group = b2d->post_integration(A);
+    if (A->get_npart_local() == 1) {
+      ASSERT_EQ(sub_group.at(1)->get_npart_local(), 1);
+    }
+
+    particle_loop(
+        sub_group.at(1),
+        [=](auto B_P, auto B_C) {
+          NESO_KERNEL_ASSERT(
+              KERNEL_ABS(B_P.at_ephemeral(0) - intersectx) < 1.0e-15, k_ep);
+          NESO_KERNEL_ASSERT(
+              KERNEL_ABS(B_P.at_ephemeral(1) - intersecty) < 1.0e-15, k_ep);
+          NESO_KERNEL_ASSERT(B_C.at_ephemeral(0) == 1, k_ep);
+          NESO_KERNEL_ASSERT(B_C.at_ephemeral(1) > -1, k_ep);
+        },
+        Access::read(BoundaryInteractionSpecification::intersection_point),
+        Access::read(BoundaryInteractionSpecification::intersection_metadata))
+        ->execute();
+    ASSERT_EQ(ep->get_flag(), 0);
+  }
+
+  b2d->free();
+  sycl_target->free();
+  A->free();
+  mesh->free();
+  PETSCCHK(DMDestroy(&dm));
+  PETSCCHK(PetscFinalize());
+}
+
+#endif
