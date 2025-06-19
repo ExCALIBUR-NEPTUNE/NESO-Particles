ac8f379f88360452ff96db7873fb6e2e80fb5e76
Will Saunders <will.saunders@ukaea.uk>
2025-06-19 14:18:32 +0100

    particle_loop interface function initially compiles.

--- include/neso_particles/particle_sub_group/particle_loop_sub_group_functions.hpp
+++ include/neso_particles/particle_sub_group/particle_loop_sub_group_functions.hpp
@@ -1,6 +1,7 @@
 #ifndef _NESO_PARTICLES_SUB_GROUP_PARTICLE_LOOP_SUB_GROUP_FUNCTIONS_HPP_
 #define _NESO_PARTICLES_SUB_GROUP_PARTICLE_LOOP_SUB_GROUP_FUNCTIONS_HPP_
 
+#include "../loop/particle_loop_reduction.hpp"
 #include "particle_loop_sub_group.hpp"
 #include "particle_loop_sub_group_reduction.hpp"
 
@@ -11,7 +12,8 @@ namespace NESO::Particles {
  * ParticleSubGroup.
  *
  *  @param name Identifier for particle loop.
- *  @param particle_group ParticleSubGroup to execute kernel for all particles.
+ *  @param particle_sub_group ParticleSubGroup to execute kernel for all
+ * particles.
  *  @param kernel Kernel to execute for all particles in the ParticleSubGroup.
  *  @param args The remaining arguments are arguments to be passed to the
  *              kernel. All arguments must be wrapped in an access descriptor
@@ -19,29 +21,46 @@ namespace NESO::Particles {
  */
 template <typename KERNEL, typename... ARGS>
 [[nodiscard]] inline ParticleLoopSharedPtr
-particle_loop(const std::string name, ParticleSubGroupSharedPtr particle_group,
-              KERNEL kernel, ARGS... args) {
-  if (particle_group->is_entire_particle_group()) {
-    auto p = std::make_shared<ParticleLoop<KERNEL, ARGS...>>(
-        name, particle_group->get_particle_group(), particle_group, kernel,
-        args...);
-    auto b = std::dynamic_pointer_cast<ParticleLoopBase>(p);
-    NESOASSERT(b != nullptr, "ParticleLoop pointer cast failed.");
-    return b;
+particle_loop(const std::string name,
+              ParticleSubGroupSharedPtr particle_sub_group, KERNEL kernel,
+              ARGS... args) {
+  ParticleLoopSharedPtr b;
+
+  if (particle_sub_group->is_entire_particle_group()) {
+    if constexpr (Access::HasReduction<ARGS...>::value) {
+      b = std::dynamic_pointer_cast<ParticleLoopBase>(
+          std::make_shared<ParticleLoopReduction<KERNEL, ARGS...>>(
+              name, particle_sub_group->get_particle_group(),
+              particle_sub_group, kernel, args...));
+    } else {
+      b = std::dynamic_pointer_cast<ParticleLoopBase>(
+          std::make_shared<ParticleLoop<KERNEL, ARGS...>>(
+              name, particle_sub_group->get_particle_group(),
+              particle_sub_group, kernel, args...));
+    }
   } else {
-    auto p = std::make_shared<ParticleLoopSubGroup<KERNEL, ARGS...>>(
-        name, particle_group, kernel, args...);
-    auto b = std::dynamic_pointer_cast<ParticleLoopBase>(p);
-    NESOASSERT(b != nullptr, "ParticleLoop pointer cast failed.");
-    return b;
+
+    if constexpr (Access::HasReduction<ARGS...>::value) {
+      b = std::dynamic_pointer_cast<ParticleLoopBase>(
+          std::make_shared<ParticleLoopSubGroupReduction<KERNEL, ARGS...>>(
+              name, particle_sub_group, kernel, args...));
+    } else {
+      b = std::dynamic_pointer_cast<ParticleLoopBase>(
+          std::make_shared<ParticleLoopSubGroup<KERNEL, ARGS...>>(
+              name, particle_sub_group, kernel, args...));
+    }
   }
+
+  NESOASSERT(b != nullptr, "ParticleLoop pointer cast failed.");
+  return b;
 }
 
 /**
  *  Create a ParticleLoop that executes a kernel for all particles in the
  * ParticleSubGroup.
  *
- *  @param particle_group ParticleSubGroup to execute kernel for all particles.
+ *  @param particle_sub_group ParticleSubGroup to execute kernel for all
+ * particles.
  *  @param kernel Kernel to execute for all particles in the ParticleSubGroup.
  *  @param args The remaining arguments are arguments to be passed to the
  *              kernel. All arguments must be wrapped in an access descriptor
@@ -49,9 +68,9 @@ particle_loop(const std::string name, ParticleSubGroupSharedPtr particle_group,
  */
 template <typename KERNEL, typename... ARGS>
 [[nodiscard]] inline ParticleLoopSharedPtr
-particle_loop(ParticleSubGroupSharedPtr particle_group, KERNEL kernel,
+particle_loop(ParticleSubGroupSharedPtr particle_sub_group, KERNEL kernel,
               ARGS... args) {
-  return particle_loop("unnamed_kernel", particle_group, kernel, args...);
+  return particle_loop("unnamed_kernel", particle_sub_group, kernel, args...);
 }
 
 } // namespace NESO::Particles
