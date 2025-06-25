07a67038461876d2a0b057c32924cd0e611dbaa0
Will Saunders <will.saunders@ukaea.uk>
2025-06-25 19:08:59 +0100

    Updated cmake to replace cmake soname linker arguments if we detect that more robust ones are available.

--- cmake/remove-file.cmake
+++
@@ -1,8 +0,0 @@
-if (FILE_TO_REMOVE)
-    if (EXISTS ${FILE_TO_REMOVE})
-        message(STATUS "File to remove exists, attempting to remove: "
-            ${FILE_TO_REMOVE})
-        file(REMOVE ${FILE_TO_REMOVE})
-    endif()
-endif()
-
