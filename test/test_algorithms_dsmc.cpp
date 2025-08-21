5f7a3b5d7a35e546026c9f7f12407de430dd7bb6
Will Saunders <will.saunders@ukaea.uk>
2025-08-21 13:00:26 +0100

    Very initial proof-of-concept looping prototype of basic N^2 DSMC.

--- test/test_algorithms_dsmc.cpp
+++ test/test_algorithms_dsmc.cpp
@@ -90,3 +90,137 @@ TEST(DSMC, cellwise_pair_list) {
 
   sycl_target->free();
 }
+
+struct PairwiseAllToAll {
+  const int workgroup_size{0};
+  const int workgroup_index{0};
+
+  PairwiseAllToAll(const int workgroup_size, const int workgroup_index)
+      : workgroup_size(workgroup_size), workgroup_index(workgroup_index) {}
+
+  template <typename KERNEL_FUNC_TYPE, typename SYNC_FUNC_TYPE>
+  inline void
+  apply_inner_block(const int workgroup_block_item,
+                    const int workgroup_block_size, const int block_start_row,
+                    const int block_start_col, const int n,
+                    KERNEL_FUNC_TYPE &kernel_func, SYNC_FUNC_TYPE &sync_func,
+                    const bool mask_required) const {
+
+    int index = workgroup_block_item;
+    const int mask = workgroup_block_size - 1;
+    if (mask_required) {
+      for (int rowx = block_start_row;
+           rowx < (block_start_row + workgroup_block_size); rowx++) {
+        const int colx = index + block_start_col;
+        if ((rowx < n) && (colx < n)) {
+          kernel_func(rowx, colx);
+        }
+        sync_func();
+        index = (index + 1) & mask;
+      }
+    } else {
+      for (int rowx = block_start_row;
+           rowx < (block_start_row + workgroup_block_size); rowx++) {
+        const int colx = index + block_start_col;
+        kernel_func(rowx, colx);
+        sync_func();
+        index = (index + 1) & mask;
+      }
+    }
+  }
+  template <typename KERNEL_FUNC_TYPE, typename SYNC_FUNC_TYPE>
+  inline void apply_diagonal(const int n, const int block_offset,
+                             KERNEL_FUNC_TYPE &kernel_func,
+                             SYNC_FUNC_TYPE &sync_func,
+                             const bool mask_required) const {
+    int size = this->workgroup_size;
+    while (size > 0) {
+      nprint(size);
+      const int workgroup_block = this->workgroup_index / size;
+      const int workgroup_block_item = this->workgroup_index % size;
+      nprint("\tworkgroup_block:", workgroup_block,
+             "workgroup_block_item:", workgroup_block_item);
+
+      int block_end_row = size + workgroup_block * 2 * size;
+      int block_start_row = block_end_row - size;
+      int block_start_col = block_end_row;
+      int block_end_col = block_start_col + size;
+
+      block_end_row += block_offset;
+      block_start_row += block_offset;
+      block_start_col += block_offset;
+      block_end_col += block_offset;
+
+      nprint("\tblock_start_row", block_start_row, "block_end_row",
+             block_end_row);
+      nprint("\tblock_start_col", block_start_col, "block_end_col",
+             block_end_col);
+
+      this->apply_inner_block(workgroup_block_item, size, block_start_row,
+                              block_start_col, n, kernel_func, sync_func,
+                              mask_required);
+
+      size /= 2;
+    }
+  }
+  template <typename KERNEL_FUNC_TYPE, typename SYNC_FUNC_TYPE>
+  inline void apply(const int n, KERNEL_FUNC_TYPE kernel_func,
+                    SYNC_FUNC_TYPE sync_func) const {
+    const int block_size_inner = this->workgroup_size;
+    const int block_size = this->workgroup_size * 2;
+    const auto n_padded = get_next_multiple(n, block_size);
+    const int num_blocks = n_padded / block_size;
+
+    for (int blockx = 0; blockx < num_blocks; blockx++) {
+      const int block_offset = blockx * block_size;
+      const bool masking_diagonal = blockx == (num_blocks - 1);
+      // Process the diagonal blocks. The last block needs masking.
+      apply_diagonal(n, block_offset, kernel_func, sync_func, masking_diagonal);
+
+      // process the rest of the blocks in the remainer of the row
+
+      const int row_start = block_offset;
+      const int row_end = block_offset + block_size;
+      const int col_start = block_offset + block_size;
+      const int col_end = n_padded;
+
+      for (int rowx = row_start; rowx < row_end; rowx += block_size_inner) {
+        for (int colx = col_start; colx < col_end; colx += block_size_inner) {
+          apply_inner_block(this->workgroup_index, this->workgroup_size, rowx,
+                            colx, n, kernel_func, sync_func,
+                            ((colx + block_size_inner) > n) ||
+                                masking_diagonal);
+        }
+      }
+    }
+  }
+};
+
+TEST(DSMC, all_to_all_looping) {
+
+  const int workgroup_size = 4;
+  const int num_particles = 7;
+
+  std::vector<int> seen_entries(num_particles * num_particles);
+  std::fill(seen_entries.begin(), seen_entries.end(), 0);
+
+  for (int workgroup_item = 0; workgroup_item < workgroup_size;
+       workgroup_item++) {
+    auto loop_context = PairwiseAllToAll(workgroup_size, workgroup_item);
+
+    loop_context.apply(
+        num_particles,
+        [&](auto i, auto j) {
+          nprint(i, j);
+          seen_entries.at(i + num_particles * j)++;
+        },
+        [&]() { nprint("sync"); });
+  }
+
+  for (int rowx = 0; rowx < num_particles; rowx++) {
+    for (int colx = 0; colx < num_particles; colx++) {
+      std::cout << seen_entries.at(rowx + num_particles * colx);
+    }
+    std::cout << std::endl;
+  }
+}
