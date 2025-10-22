aed3b09ae80023b034577c2224ce9a4ec5de7faf
Will Saunders <will.saunders@ukaea.uk>
2025-10-22 12:54:07 +0100

    Moved global info creation into ParticlePairLoopArgs.

--- test/test_particle_pair_loop_a.cpp
+++ test/test_particle_pair_loop_a.cpp
@@ -129,11 +129,71 @@ protected:
         kernel_masks_type, iteration_A, iteration_B, loop_args, kernel_args);
   }
 
+  ParticleGroupSharedPtr particle_group_A{nullptr};
+  ParticleGroupSharedPtr particle_group_B{nullptr};
+  ParticleSubGroupSharedPtr particle_sub_group_A{nullptr};
+  ParticleSubGroupSharedPtr particle_sub_group_B{nullptr};
+
+  inline void create_global_info(
+      const std::optional<int> cell_start = std::nullopt,
+      const std::optional<int> cell_end = std::nullopt,
+      ParticleLoopImplementation::ParticleLoopGlobalInfo *global_info_A =
+          nullptr,
+      ParticleLoopImplementation::ParticleLoopGlobalInfo *global_info_B =
+          nullptr) {
+
+    int cell_start_actual = 0;
+    int cell_end_actual =
+        this->particle_group_A->domain->mesh->get_cell_count();
+
+    bool all_cells = true;
+    if (cell_start != std::nullopt) {
+      cell_start_actual = cell_start.value();
+      all_cells = false;
+    }
+    if (cell_end != std::nullopt) {
+      cell_end_actual = cell_end.value();
+      all_cells = false;
+    }
+
+    const auto local_size =
+        this->sycl_target->parameters
+            ->template get<SizeTParameter>("LOOP_LOCAL_SIZE")
+            ->value;
+
+    global_info_A->particle_group = this->particle_group_A.get();
+    global_info_A->particle_sub_group =
+        this->particle_sub_group_A ? this->particle_sub_group_A.get() : nullptr;
+    global_info_A->all_cells = all_cells;
+    global_info_A->starting_cell = cell_start_actual;
+    global_info_A->bounding_cell = cell_end_actual;
+    global_info_A->local_size = local_size;
+
+    global_info_B->particle_group = this->particle_group_B.get();
+    global_info_B->particle_sub_group =
+        this->particle_sub_group_B ? this->particle_sub_group_B.get() : nullptr;
+    global_info_B->all_cells = all_cells;
+    global_info_B->starting_cell = cell_start_actual;
+    global_info_B->bounding_cell = cell_end_actual;
+    global_info_B->local_size = local_size;
+  }
+
 public:
+  SYCLTargetSharedPtr sycl_target{nullptr};
   std::string name;
   virtual ~ParticlePairLoopArgs<ARGS...>() = default;
 
-  ParticlePairLoopArgs<ARGS...>(std::string &name, ARGS... args) : name(name) {
+  ParticlePairLoopArgs<ARGS...>(SYCLTargetSharedPtr sycl_target,
+                                std::string &name,
+                                ParticleGroupSharedPtr particle_group_A,
+                                ParticleGroupSharedPtr particle_group_B,
+                                ParticleSubGroupSharedPtr particle_sub_group_A,
+                                ParticleSubGroupSharedPtr particle_sub_group_B,
+                                ARGS... args)
+      : particle_group_A(particle_group_A), particle_group_B(particle_group_B),
+        particle_sub_group_A(particle_sub_group_A),
+        particle_sub_group_B(particle_sub_group_B), sycl_target(sycl_target),
+        name(name) {
     this->unpack_args<0>(args...);
   }
 };
@@ -179,7 +239,6 @@ protected:
   using ParticlePairLoopArgs<ARGS...>::create_loop_args;
   using ParticlePairLoopArgs<ARGS...>::create_kernel_args;
 
-  SYCLTargetSharedPtr sycl_target;
   std::vector<DSMC::CellwisePairListDevice> h_pair_lists_device;
   std::shared_ptr<BufferDevice<DSMC::CellwisePairListDevice>>
       d_pair_lists_device;
@@ -195,14 +254,24 @@ public:
       std::string name,
       std::vector<CellwisePairListAbsolute<ParticleGroup>> pair_lists,
       ParticlePairLoopKernel<KERNEL> kernel, ARGS... args)
-      : ParticlePairLoopArgs<ARGS...>(name, args...), pair_lists(pair_lists),
-        kernel(kernel) {
+      : ParticlePairLoopArgs<ARGS...>(nullptr, name, nullptr, nullptr, nullptr,
+                                      nullptr, args...),
+        pair_lists(pair_lists), kernel(kernel) {
 
     if (pair_lists.size()) {
       this->num_pair_lists = pair_lists.size();
-      auto particle_group = get_particle_group(pair_lists[0].A);
-      this->sycl_target = particle_group->sycl_target;
-      this->cell_count = particle_group->domain->mesh->get_cell_count();
+      this->particle_group_A = get_particle_group(pair_lists[0].A);
+      this->particle_group_B = get_particle_group(pair_lists[0].B);
+      NESOASSERT(this->particle_group_A->domain ==
+                     this->particle_group_B->domain,
+                 "Domain missmatch between ParticleGroups.");
+
+      NESOASSERT(this->particle_group_A->sycl_target ==
+                     this->particle_group_B->sycl_target,
+                 "Compute device missmatch between ParticleGroups.");
+
+      this->sycl_target = this->particle_group_A->sycl_target;
+      this->cell_count = this->particle_group_A->domain->mesh->get_cell_count();
       NESOASSERT(this->sycl_target != nullptr, "Bad compute device found");
       this->d_pair_lists_device =
           std::make_shared<BufferDevice<DSMC::CellwisePairListDevice>>(
@@ -233,39 +302,34 @@ public:
     }
     auto e0 = this->d_pair_lists_device->set_async(this->h_pair_lists_device);
 
-    // START OF IMPLEMENTATION TO MOVE INTO A BASE CLASS
-    int cell_start_actual = 0;
-    int cell_end_actual = this->cell_count;
-    if (cell_start != std::nullopt) {
-      cell_start_actual = cell_start.value();
-    }
-    if (cell_end != std::nullopt) {
-      cell_end_actual = cell_end.value();
-    }
-    const int cell_count_iteration = cell_end_actual - cell_start_actual;
-
     ParticleLoopImplementation::ParticleLoopGlobalInfo global_info_A;
     ParticleLoopImplementation::ParticleLoopGlobalInfo global_info_B;
 
-    global_info_A.particle_group = get_particle_group(pair_lists[0].A).get();
-    global_info_A.particle_sub_group = nullptr;
-    global_info_B.particle_group = get_particle_group(pair_lists[0].B).get();
-    global_info_B.particle_sub_group = nullptr;
+    this->create_global_info(cell_start, cell_end, &global_info_A,
+                             &global_info_B);
 
-    // END OF IMPLEMENTATION TO MOVE INTO A BASE CLASS
+    const auto cell_start_actual = global_info_A.starting_cell;
+    const auto cell_end_actual = global_info_A.bounding_cell;
+    const int cell_count_iteration = cell_end_actual - cell_start_actual;
 
     e0.wait_and_throw();
     if ((max_pair_count > 0) && (this->num_pair_lists > 0) &&
         (cell_count_iteration > 0)) {
 
-      auto iteration_set =
-          this->sycl_target->device_limits.validate_range_global(
-              sycl::range<3>(static_cast<std::size_t>(cell_count_iteration),
-                             static_cast<std::size_t>(this->num_pair_lists),
-                             static_cast<std::size_t>(max_pair_count)));
-
       auto k_kernel = this->kernel.kernel;
       auto k_pair_lists = this->d_pair_lists_device->ptr;
+      std::size_t local_size = global_info_A.local_size;
+
+      sycl::range<3> local_iteration_set(1, 1, local_size);
+      sycl::range<3> global_iteration_set(
+          static_cast<std::size_t>(cell_count_iteration),
+          static_cast<std::size_t>(this->num_pair_lists),
+          static_cast<std::size_t>(
+              get_next_multiple(max_pair_count, local_size)));
+
+      sycl::nd_range<3> nd_iteration_set =
+          this->sycl_target->device_limits.validate_nd_range(
+              sycl::nd_range<3>(global_iteration_set, local_iteration_set));
 
       this->event_stack.push(
           this->sycl_target->queue.submit([&](sycl::handler &cgh) {
@@ -274,10 +338,12 @@ public:
             this->create_loop_args(cgh, loop_args, kernel_masks, &global_info_A,
                                    &global_info_B);
 
-            cgh.parallel_for(iteration_set, [=](sycl::item<3> idx) {
-              const std::size_t index_cell = idx.get_id(0);
-              const std::size_t index_list = idx.get_id(1);
-              const std::size_t index_pair = idx.get_id(2);
+            cgh.parallel_for(nd_iteration_set, [=](sycl::nd_item<3> idx) {
+              const std::size_t index_cell =
+                  idx.get_global_id(0) + cell_start_actual;
+              const std::size_t index_list = idx.get_global_id(1);
+              const std::size_t index_pair = idx.get_global_id(2);
+
               const auto *pair_list = &k_pair_lists[index_list];
               const auto num_pairs = static_cast<std::size_t>(
                   pair_list->d_pair_counts[index_cell]);
@@ -364,11 +430,11 @@ TEST(ParticlePairLoop, base) {
   ParticlePairLoopCellwisePairList pl0(
       "particle_pair_loop_test",
       {CellwisePairListAbsolute<ParticleGroup>(A, A, cellwise_pair_listA)},
-      particle_pair_loop_kernel([](auto ID_A, auto ID_B, auto NN_A, auto NN_B) {
-        NN_A.at(0) = ID_B.at(0);
-        NN_B.at(0) = ID_A.at(0);
-        NN_A.at(1)++;
-        NN_B.at(1)++;
+      particle_pair_loop_kernel([](auto ID_i, auto ID_j, auto NN_i, auto NN_j) {
+        NN_i.at(0) = ID_j.at(0);
+        NN_j.at(0) = ID_i.at(0);
+        NN_i.at(1)++;
+        NN_j.at(1)++;
       }),
       Access::A(Access::read(Sym<INT>("ID"))),
       Access::B(Access::read(Sym<INT>("ID"))),
@@ -395,6 +461,102 @@ TEST(ParticlePairLoop, base) {
     ASSERT_EQ(NN->at(index_j, 1), 2);
     ASSERT_EQ(neighbour_j, id_i);
     ASSERT_EQ(neighbour_i, id_j);
+  }
+
+  sycl_target->free();
+}
+
+TEST(ParticlePairLoop, foo) {
+
+  int npart_cell = 1000;
+  const int ndim = 2;
+  const int nx = 16;
+  const int ny = 33;
+  const int nz = 48;
+
+  auto [A, sycl_target, cell_count] =
+      particle_loop_create_common(npart_cell, ndim, nx, ny, nz);
+  A->add_particle_dat(Sym<REAL>("FORCE"), 2);
+
+  particle_loop(
+      A,
+      [=](auto FORCE) {
+        FORCE.at(0) = 0;
+        FORCE.at(1) = 1;
+      },
+      Access::write(Sym<REAL>("FORCE")))
+      ->execute();
+
+  auto cellwise_pair_listA =
+      std::make_shared<DSMC::CellwisePairList>(sycl_target, cell_count);
+
+  std::vector<int> c;
+  std::vector<int> i;
+  std::vector<int> j;
+
+  c.reserve(cell_count * npart_cell / 2);
+  i.reserve(cell_count * npart_cell / 2);
+  j.reserve(cell_count * npart_cell / 2);
+
+  std::mt19937 rng(9124234 + sycl_target->comm_pair.rank_parent);
+
+  for (int cellx = 0; cellx < cell_count; cellx++) {
+    npart_cell = A->get_npart_cell(cellx);
+    std::vector<int> pairs(npart_cell);
+    std::iota(pairs.begin(), pairs.end(), 0);
+    std::shuffle(pairs.begin(), pairs.end(), rng);
+    for (int px = 0; px < (npart_cell / 2); px++) {
+      c.push_back(cellx);
+      i.push_back(pairs.at(2 * px));
+      j.push_back(pairs.at(2 * px + 1));
+    }
+  }
+
+  cellwise_pair_listA->push_back(c, i, j);
+
+  constexpr REAL sigma = 1.0;
+  constexpr REAL sigma2 = sigma * sigma;
+  constexpr REAL epsilon = 1.0;
+  constexpr REAL CF = -48.0 * epsilon / sigma2;
+
+  ParticlePairLoopCellwisePairList pl0(
+      "particle_pair_loop_test",
+      {CellwisePairListAbsolute<ParticleGroup>(A, A, cellwise_pair_listA)},
+      particle_pair_loop_kernel(
+          [](auto P_i, auto P_j, auto FORCE_i, auto FORCE_j) {
+            const double R0 = P_j.at(0) - P_i.at(0);
+            const double R1 = P_j.at(1) - P_i.at(1);
+
+            // distance squared
+            const double r2 = R0 * R0 + R1 * R1;
+
+            // (sigma/r)**2, (sigma/r)**4 and  (sigma/r)**6
+            const double r_m2 = sigma2 / r2;
+            const double r_m4 = r_m2 * r_m2;
+            const double r_m6 = r_m4 * r_m2;
+
+            // (sigma/r)**8
+            const double r_m8 = r_m4 * r_m4;
+
+            // compute force magnitude
+            const double f_tmp = CF * (r_m6 - 0.5) * r_m8;
+
+            // increment force on particle i
+            FORCE_i.at(0) = f_tmp * R0;
+            FORCE_i.at(1) = f_tmp * R1;
+            FORCE_j.at(0) = -f_tmp * R0;
+            FORCE_j.at(1) = -f_tmp * R1;
+          }),
+      Access::A(Access::read(Sym<REAL>("P"))),
+      Access::B(Access::read(Sym<REAL>("P"))),
+      Access::A(Access::write(Sym<REAL>("FORCE"))),
+      Access::B(Access::write(Sym<REAL>("FORCE"))));
+
+  nprint("start");
+
+  for (int stepx = 0; stepx < 1000000; stepx++) {
+    nprint(stepx);
+    pl0.execute();
   }
 
   sycl_target->free();
