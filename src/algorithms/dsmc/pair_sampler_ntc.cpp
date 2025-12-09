7bcdab0ddc62813e529ef3ce572f30261097a69c
Will Saunders <will.saunders@ukaea.uk>
2025-12-09 15:05:49 +0000

    CPU sampling approach that may avoid acpp bugs.

--- src/algorithms/dsmc/pair_sampler_ntc.cpp
+++ src/algorithms/dsmc/pair_sampler_ntc.cpp
@@ -126,150 +126,233 @@ void PairSamplerNTC::sample(ParticleSubGroupSharedPtr sub_group_a,
              "Required local memory size is less than available local "
              "memory. Please raise an issue if this is actually a problem.");
 
-  auto nd_iteration_set = this->sycl_target->device_limits.validate_nd_range(
-      sycl::nd_range<2>(sycl::range<2>(this->cell_count, this->block_size),
-                        sycl::range<2>(1, this->block_size)));
-
   const auto k_cell_count = this->cell_count;
-  this->sycl_target->queue
-      .submit([&](sycl::handler &cgh) {
-        sycl::local_accessor<int, 1> la_counts(sycl::range(1), cgh);
-        sycl::local_accessor<int, 1> la_flags(sycl::range(max_npart_cell), cgh);
 
-        cgh.parallel_for(nd_iteration_set, [=](sycl::nd_item<2> idx) {
-          const int cell = idx.get_global_id(0);
-          const int local_id = idx.get_group().get_local_linear_id();
-          const int local_range = idx.get_group().get_local_linear_range();
-          const int npart_cell = k_npart_cell[cell];
-          const int npairs = k_pair_counts[cell];
-          const int npart_cell_a = selection_a.d_npart_cell[cell];
-          const int npart_cell_b = selection_b.d_npart_cell[cell];
-          const int sample_range_a = npart_cell_a;
-          const int sample_range_b = a_is_b ? npart_cell_a - 1 : npart_cell_b;
-          const REAL scale_a = static_cast<REAL>(sample_range_a);
-          const REAL scale_b = static_cast<REAL>(sample_range_b);
-          const int offset_cell = k_pair_counts_es[cell];
-          int max_wave_count = 0;
-          int block_index = 0;
+  if (this->sycl_target->device.is_cpu()) {
 
-          if (local_id == 0) {
+    auto nd_iteration_set = this->sycl_target->device_limits.validate_nd_range(
+        sycl::nd_range<1>(sycl::range<1>(this->cell_count), sycl::range<1>(1)));
+
+    this->sycl_target->queue
+        .submit([&](sycl::handler &cgh) {
+          sycl::local_accessor<int, 1> la_counts(sycl::range(1), cgh);
+          sycl::local_accessor<int, 1> la_flags(sycl::range(max_npart_cell),
+                                                cgh);
+
+          cgh.parallel_for(nd_iteration_set, [=](sycl::nd_item<1> idx) {
+            const int cell = idx.get_global_id(0);
+            const int npart_cell = k_npart_cell[cell];
+            const int npairs = k_pair_counts[cell];
+            const int npart_cell_a = selection_a.d_npart_cell[cell];
+            const int npart_cell_b = selection_b.d_npart_cell[cell];
+            const int sample_range_a = npart_cell_a;
+            const int sample_range_b = a_is_b ? npart_cell_a - 1 : npart_cell_b;
+            const REAL scale_a = static_cast<REAL>(sample_range_a);
+            const REAL scale_b = static_cast<REAL>(sample_range_b);
+            const int offset_cell = k_pair_counts_es[cell];
+            int max_wave_count = 0;
+
             la_counts[0] = 0;
-          }
-          for (int ix = local_id; ix < npart_cell; ix += local_range) {
-            la_flags[ix] = 0;
-          }
+            for (int ix = 0; ix < npart_cell; ix++) {
+              la_flags[ix] = 0;
+            }
 
-          idx.barrier(sycl::access::fence_space::local_space);
+            const int num_blocks = div_round_up(npairs, block_size);
 
-          for (int num_sampled_pairs = 0; num_sampled_pairs < npairs;
-               num_sampled_pairs += local_range) {
+            for (int block_index = 0; block_index < num_blocks; block_index++) {
+              const int px_start = block_index * block_size;
+              const int px_end = sycl::min(px_start + block_size, npairs);
+              for (int px = px_start; px < px_end; px++) {
+                const int ox = offset_cell + px;
 
-            int i3 = -1;
-            int j3 = -1;
-            int wave = -1000000000;
-            int order = 0;
-            const int ox = offset_cell + num_sampled_pairs + local_id;
+                const int i0 = scale_a * k_real_samples[ox];
+                const int j0 = scale_b * k_real_samples[k_pair_count + ox];
+                const int i1 = Kernel::min(i0, sample_range_a - 1);
+                const int j1 = Kernel::min(j0, sample_range_b - 1);
 
-            if ((num_sampled_pairs + local_id) < npairs) {
+                const int i2 = i1;
+                const int j2 = a_is_b ? (i1 + 1 + j1) % npart_cell_a : j1;
 
-              const int i0 = scale_a * k_real_samples[ox];
-              const int j0 = scale_b * k_real_samples[k_pair_count + ox];
-              const int i1 = Kernel::min(i0, sample_range_a - 1);
-              const int j1 = Kernel::min(j0, sample_range_b - 1);
+                const int i3 = selection_a.d_map_cells_to_particles
+                                   .map_loop_layer_to_layer(cell, i2);
+                const int j3 = selection_b.d_map_cells_to_particles
+                                   .map_loop_layer_to_layer(cell, j2);
 
-              const int i2 = i1;
-              const int j2 = a_is_b ? (i1 + 1 + j1) % npart_cell_a : j1;
+                const int wave_i = la_flags[i3];
+                const int wave_j = la_flags[j3];
+                const int wave = sycl::max(wave_i, wave_j);
+                la_flags[i3] = wave + 1;
+                la_flags[j3] = wave + 1;
+                k_pair_list[ox] = i3;
+                k_pair_list[k_pair_count * 1 + ox] = j3;
+                k_pair_list[k_pair_count * 2 + ox] = wave;
+                max_wave_count = sycl::max(wave + 1, max_wave_count);
+              }
 
-              i3 = selection_a.d_map_cells_to_particles.map_loop_layer_to_layer(
-                  cell, i2);
-              j3 = selection_b.d_map_cells_to_particles.map_loop_layer_to_layer(
-                  cell, j2);
+              for (int px = px_start; px < px_end; px++) {
 
-              const int flag_i =
-                  sycl::atomic_ref<int, sycl::memory_order::relaxed,
-                                   sycl::memory_scope::work_group>(la_flags[i3])
-                      .fetch_add(1);
-              const int flag_j =
-                  sycl::atomic_ref<int, sycl::memory_order::relaxed,
-                                   sycl::memory_scope::work_group>(la_flags[j3])
-                      .fetch_add(1);
-
-              // If both flags are zero then either there are no conflicts or
-              // this work item managed to be the first to flag both indices
-              // before any other work item.
-              if ((flag_i == 0) && (flag_j == 0)) {
-                wave = 0;
-              } else {
-                order = sycl::atomic_ref<int, sycl::memory_order::relaxed,
-                                         sycl::memory_scope::work_group>(
-                            la_counts[0])
-                            .fetch_add(1) +
-                        1;
+                const int ox = offset_cell + px;
+                const int i3 = k_pair_list[ox];
+                const int j3 = k_pair_list[k_pair_count * 1 + ox];
+                la_flags[i3] = 0;
+                la_flags[j3] = 0;
               }
             }
 
-            // resolve dependencies within the block of pairs
-            idx.barrier(sycl::access::fence_space::local_space);
-            const int num_steps_ordering = la_counts[0];
+            atomic_fetch_max(k_max_wave_count, max_wave_count);
+          });
+        })
+        .wait_and_throw();
 
-            if (num_steps_ordering) {
+  } else {
 
-              for (int orderx = 0; orderx < num_steps_ordering; orderx++) {
-                if ((orderx + 1) == order) {
-                  la_flags[i3] = 0;
-                  la_flags[j3] = 0;
-                }
-                idx.barrier(sycl::access::fence_space::local_space);
-              }
-              idx.barrier(sycl::access::fence_space::local_space);
+    auto nd_iteration_set = this->sycl_target->device_limits.validate_nd_range(
+        sycl::nd_range<2>(sycl::range<2>(this->cell_count, this->block_size),
+                          sycl::range<2>(1, this->block_size)));
 
-              if (wave > -1) {
-                la_flags[i3] = 1;
-                la_flags[j3] = 1;
-              }
-              idx.barrier(sycl::access::fence_space::local_space);
+    this->sycl_target->queue
+        .submit([&](sycl::handler &cgh) {
+          sycl::local_accessor<int, 1> la_counts(sycl::range(1), cgh);
+          sycl::local_accessor<int, 1> la_flags(sycl::range(max_npart_cell),
+                                                cgh);
 
-              for (int orderx = 0; orderx < num_steps_ordering; orderx++) {
-
-                // Only one of the work items holding a conflicting pair should
-                // enter this conditional per iteration.
-                if ((orderx + 1) == order) {
-                  wave = sycl::max(la_flags[i3], la_flags[j3]);
-                  la_flags[i3] = wave + 1;
-                  la_flags[j3] = wave + 1;
-                  la_counts[0] = sycl::max(wave, la_counts[0]);
-                }
-
-                idx.barrier(sycl::access::fence_space::local_space);
-              }
-
-              idx.barrier(sycl::access::fence_space::local_space);
-            }
-            if (wave >= 0) {
-              k_pair_list[ox] = i3;
-              k_pair_list[k_pair_count * 1 + ox] = j3;
-              k_pair_list[k_pair_count * 2 + ox] = wave;
-              max_wave_count = sycl::max(max_wave_count, wave + 1);
-            }
+          cgh.parallel_for(nd_iteration_set, [=](sycl::nd_item<2> idx) {
+            const int cell = idx.get_global_id(0);
+            const int local_id = idx.get_group().get_local_linear_id();
+            const int local_range = idx.get_group().get_local_linear_range();
+            const int npart_cell = k_npart_cell[cell];
+            const int npairs = k_pair_counts[cell];
+            const int npart_cell_a = selection_a.d_npart_cell[cell];
+            const int npart_cell_b = selection_b.d_npart_cell[cell];
+            const int sample_range_a = npart_cell_a;
+            const int sample_range_b = a_is_b ? npart_cell_a - 1 : npart_cell_b;
+            const REAL scale_a = static_cast<REAL>(sample_range_a);
+            const REAL scale_b = static_cast<REAL>(sample_range_b);
+            const int offset_cell = k_pair_counts_es[cell];
+            int max_wave_count = 0;
+            int block_index = 0;
 
             if (local_id == 0) {
-              k_wave_counts[block_index * k_cell_count + cell] =
-                  la_counts[0] + 1;
               la_counts[0] = 0;
             }
-            if (i3 > -1) {
-              la_flags[i3] = 0;
-              la_flags[j3] = 0;
+            for (int ix = local_id; ix < npart_cell; ix += local_range) {
+              la_flags[ix] = 0;
             }
 
             idx.barrier(sycl::access::fence_space::local_space);
-            block_index++;
-          }
 
-          atomic_fetch_max(k_max_wave_count, max_wave_count);
-        });
-      })
-      .wait_and_throw();
+            for (int num_sampled_pairs = 0; num_sampled_pairs < npairs;
+                 num_sampled_pairs += local_range) {
+
+              int i3 = -1;
+              int j3 = -1;
+              int wave = -1000000000;
+              int order = 0;
+              const int ox = offset_cell + num_sampled_pairs + local_id;
+
+              if ((num_sampled_pairs + local_id) < npairs) {
+
+                const int i0 = scale_a * k_real_samples[ox];
+                const int j0 = scale_b * k_real_samples[k_pair_count + ox];
+                const int i1 = Kernel::min(i0, sample_range_a - 1);
+                const int j1 = Kernel::min(j0, sample_range_b - 1);
+
+                const int i2 = i1;
+                const int j2 = a_is_b ? (i1 + 1 + j1) % npart_cell_a : j1;
+
+                i3 = selection_a.d_map_cells_to_particles
+                         .map_loop_layer_to_layer(cell, i2);
+                j3 = selection_b.d_map_cells_to_particles
+                         .map_loop_layer_to_layer(cell, j2);
+
+                const int flag_i =
+                    sycl::atomic_ref<int, sycl::memory_order::relaxed,
+                                     sycl::memory_scope::work_group>(
+                        la_flags[i3])
+                        .fetch_add(1);
+                const int flag_j =
+                    sycl::atomic_ref<int, sycl::memory_order::relaxed,
+                                     sycl::memory_scope::work_group>(
+                        la_flags[j3])
+                        .fetch_add(1);
+
+                // If both flags are zero then either there are no conflicts or
+                // this work item managed to be the first to flag both indices
+                // before any other work item.
+                if ((flag_i == 0) && (flag_j == 0)) {
+                  wave = 0;
+                } else {
+                  order = sycl::atomic_ref<int, sycl::memory_order::relaxed,
+                                           sycl::memory_scope::work_group>(
+                              la_counts[0])
+                              .fetch_add(1) +
+                          1;
+                }
+              }
+
+              // resolve dependencies within the block of pairs
+              idx.barrier(sycl::access::fence_space::local_space);
+              const int num_steps_ordering = la_counts[0];
+
+              if (num_steps_ordering) {
+
+                for (int orderx = 0; orderx < num_steps_ordering; orderx++) {
+                  if ((orderx + 1) == order) {
+                    la_flags[i3] = 0;
+                    la_flags[j3] = 0;
+                  }
+                  idx.barrier(sycl::access::fence_space::local_space);
+                }
+                idx.barrier(sycl::access::fence_space::local_space);
+
+                if (wave > -1) {
+                  la_flags[i3] = 1;
+                  la_flags[j3] = 1;
+                }
+                idx.barrier(sycl::access::fence_space::local_space);
+
+                for (int orderx = 0; orderx < num_steps_ordering; orderx++) {
+
+                  // Only one of the work items holding a conflicting pair
+                  // should enter this conditional per iteration.
+                  if ((orderx + 1) == order) {
+                    wave = sycl::max(la_flags[i3], la_flags[j3]);
+                    la_flags[i3] = wave + 1;
+                    la_flags[j3] = wave + 1;
+                    la_counts[0] = sycl::max(wave, la_counts[0]);
+                  }
+
+                  idx.barrier(sycl::access::fence_space::local_space);
+                }
+
+                idx.barrier(sycl::access::fence_space::local_space);
+              }
+              if (wave >= 0) {
+                k_pair_list[ox] = i3;
+                k_pair_list[k_pair_count * 1 + ox] = j3;
+                k_pair_list[k_pair_count * 2 + ox] = wave;
+                max_wave_count = sycl::max(max_wave_count, wave + 1);
+              }
+
+              if (local_id == 0) {
+                k_wave_counts[block_index * k_cell_count + cell] =
+                    la_counts[0] + 1;
+                la_counts[0] = 0;
+              }
+              if (i3 > -1) {
+                la_flags[i3] = 0;
+                la_flags[j3] = 0;
+              }
+
+              idx.barrier(sycl::access::fence_space::local_space);
+              block_index++;
+            }
+
+            atomic_fetch_max(k_max_wave_count, max_wave_count);
+          });
+        })
+        .wait_and_throw();
+  }
 
   this->sycl_target->queue
       .memcpy(&this->max_wave_count, k_max_wave_count, sizeof(int))
