9bda8745967db23960bfdeedbd52f5ea18b7d0cd
Will Saunders <will.saunders@ukaea.uk>
2026-04-29 12:13:42 +0100

    Added initial implementation of Triad(REAL) benchmark.

--- test/test_benchmarks.cpp
+++ test/test_benchmarks.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include "include/test_neso_particles.hpp"
 #include <iomanip>
 #include <iostream>
 #include <neso_particles.hpp>
@@ -106,5 +107,102 @@ TEST(Benchmark, bandwidth_device_copy) {
     }
     MPICHK(MPI_Barrier(MPI_COMM_WORLD));
     sycl_target->free();
+  }
+}
+
+TEST(Benchmark, bandwidth_triad_REAL) {
+  if (benchmark_enabled) {
+
+    const int ndim = 2;
+    std::vector<int> dims(ndim);
+    dims[0] = 32;
+    dims[1] = 32;
+
+    auto mesh =
+        std::make_shared<CartesianHMesh>(MPI_COMM_WORLD, ndim, dims, 1, 0);
+    auto sycl_target =
+        std::make_shared<SYCLTarget>(GPU_SELECTOR, mesh->get_comm());
+    auto cart_local_mapper = CartesianHMeshLocalMapper(sycl_target, mesh);
+    auto domain = std::make_shared<Domain>(mesh, cart_local_mapper);
+
+    ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
+                               ParticleProp(Sym<INT>("CELL_ID"), 1, true),
+                               ParticleProp(Sym<REAL>("A"), 1),
+                               ParticleProp(Sym<REAL>("B"), 3)};
+    auto A =
+        std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
+    const int cell_count = mesh->get_cell_count();
+
+    const int comm_size = sycl_target->comm_pair.size_parent;
+    const int comm_rank = sycl_target->comm_pair.rank_parent;
+    const bool root = sycl_target->comm_pair.rank_parent == 0;
+    const int Ntest = 400;
+
+    std::ofstream out_stream;
+    if (root) {
+      out_stream = std::ofstream("benchmark_bandwidth_triad_REAL.csv");
+    }
+
+    if (root) {
+      auto lambda_print = [&](auto &os) {
+        os << "Size (Bytes), Bandwidth (GB/s), Time Taken (s)" << std::endl;
+      };
+      lambda_print(std::cout);
+      lambda_print(out_stream);
+    }
+    for (int px = 0; px < 16; px++) {
+      const std::size_t N =
+          std::pow(static_cast<std::size_t>(2), static_cast<std::size_t>(px)) *
+          dims[0] * dims[1];
+
+      std::size_t start = 0;
+      std::size_t end = 0;
+      get_decomp_1d(comm_size, N, comm_rank, &start, &end);
+      std::size_t Nlocal = end - start;
+
+      A->clear();
+      ParticleSet initial_distribution(Nlocal, particle_spec);
+      for (std::size_t px = 0; px < Nlocal; px++) {
+        initial_distribution[Sym<INT>("CELL_ID")][px][0] = px % cell_count;
+      }
+
+      A->add_particles_local(initial_distribution);
+
+      auto loop = particle_loop(
+          A, [=](auto A, auto B) { A.at(0) = B.at(0) + B.at(1) * B.at(2); },
+          Access::write(Sym<REAL>("A")), Access::read(Sym<REAL>("B")));
+
+      MPICHK(MPI_Barrier(MPI_COMM_WORLD));
+      auto t0 = profile_timestamp();
+      for (int testx = 0; testx < Ntest; testx++) {
+        loop->execute();
+      }
+      MPICHK(MPI_Barrier(MPI_COMM_WORLD));
+      auto t1 = profile_timestamp();
+      auto time_taken = profile_elapsed(t0, t1);
+
+      const std::size_t num_bytes = N * 4 * sizeof(REAL);
+      auto bw = Ntest * num_bytes / time_taken;
+      bw /= 1e9;
+
+      if (root) {
+        auto lambda_print = [&](auto &os) {
+          os << std::setfill(' ') << std::setw(12) << num_bytes << ","
+             << std::setw(16) << std::scientific << bw << "," << std::setw(16)
+             << std::scientific << time_taken << std::endl;
+        };
+        lambda_print(std::cout);
+        lambda_print(out_stream);
+      }
+    }
+
+    std::cout << std::flush;
+    if (root) {
+      out_stream << std::flush;
+      out_stream.close();
+    }
+    MPICHK(MPI_Barrier(MPI_COMM_WORLD));
+    sycl_target->free();
+    mesh->free();
   }
 }
