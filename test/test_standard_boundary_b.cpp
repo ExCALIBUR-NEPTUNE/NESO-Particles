37cdf7c54677cfc194443a697496f764e4aa30c2
Will Saunders <will.saunders@ukaea.uk>
2026-08-24 17:18:17 +0100

    Start of implementing test case.

--- test/test_standard_boundary_b.cpp
+++ test/test_standard_boundary_b.cpp
@@ -257,5 +257,88 @@ TEST(CartesianTrajectoryIntersection, offsets_3d) {
   lambda_test(aa, 0.0, -100.0, 0.0, 1, 0.0, 0, 2);
   lambda_test(aa, 0.0, 0.0, -100.0, 2, 0.0, 0, 1);
 
+  cartesian_trajectory_intersection->free();
   sycl_target->free();
+  A->domain->mesh->free();
+}
+
+
+TEST(CartesianTrajectoryIntersection, shallow_intersection_3d) {
+
+  const int ncell_x = 12;
+  const int ncell_y = 7;
+  const int ncell_z = 8;
+
+  auto [A_t, sycl_target_t, cell_count_t] =
+      particle_loop_common_3d(4, ncell_x, ncell_y, ncell_z);
+
+  auto sycl_target = sycl_target_t;
+  auto A = A_t;
+
+  const int ndim = 3;
+  std::map<int, std::vector<int>> boundary_groups;
+  boundary_groups[0] = {0, 2};
+  boundary_groups[1] = {1, 3};
+  boundary_groups[2] = {4, 5};
+
+  auto cartesian_trajectory_intersection =
+      std::make_shared<CartesianTrajectoryIntersection>(
+          sycl_target,
+          std::dynamic_pointer_cast<CartesianHMesh>(A->domain->mesh),
+          boundary_groups, 1.0e-10);
+  cartesian_trajectory_intersection->prepare_particle_group(A);
+
+
+
+  {
+    auto aa = particle_sub_group(
+        A, [=](auto INDEX) { return INDEX.get_loop_linear_index() != 0; },
+        Access::read(ParticleLoopIndex{}));
+
+    A->remove_particles(aa);
+    ASSERT_EQ(A->get_npart_local(), 1);
+  }
+  
+  particle_loop(
+    A,
+    [=](auto P){
+      P.at(0) = 1.0;
+      P.at(1) = 1.0;
+      P.at(2) = 1e-14;
+    },
+    Access::write(Sym<REAL>("P"))
+  )->execute();
+
+  cartesian_trajectory_intersection->pre_integration(A);
+
+  A->print(Sym<REAL>("P"), Sym<REAL>("NESO_PARTICLES_CART_H_MESH_PREVIOUS_POS"));
+
+   particle_loop(
+    A,
+    [=](auto P){
+      P.at(0) = 1.0;
+      P.at(1) = -1.0;
+      P.at(2) = -1.0e-14;
+    },
+    Access::write(Sym<REAL>("P"))
+  )->execute(); 
+
+
+
+  A->print(Sym<REAL>("P"), Sym<REAL>("NESO_PARTICLES_CART_H_MESH_PREVIOUS_POS"));
+
+  auto groups =
+      cartesian_trajectory_intersection->post_integration(A);
+
+
+
+
+  for(auto & gx : groups){
+    nprint(gx.first, gx.second->get_npart_local());
+  }
+
+
+  cartesian_trajectory_intersection->free();
+  sycl_target->free();
+  A->domain->mesh->free();
 }
